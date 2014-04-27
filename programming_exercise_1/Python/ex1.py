769b6d233dff80d501bf4ba1b762873502aebb29
cklcit03 <flashbuzzer@gmail.com>
2014-04-26 22:35:05 -0400

    added display/plot functions

--- programming_exercise_1/Python/ex1.py
+++ programming_exercise_1/Python/ex1.py
@@ -2,43 +2,73 @@
 # Programming Exercise 1: Linear Regression
 # Problem: Predict profits for a food truck given data for profits/populations of various cities
 # Linear regression with one variable
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from matplotlib import rc
+from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
+
+class InsufficientIterations(Exception):
+    def __init__(self,value):
+        self.value = value
+    def __str__(self):
+        return repr(self.value)
+
+class InsufficientTrainingExamples(Exception):
+    def __init__(self,value):
+        self.value = value
+    def __str__(self):
+        return repr(self.value)
+
+# Plot data
+def plotData(x,y):
+    "Plot data"
+    plt.plot(x,y,'rx',markersize=18)
+    plt.ylabel('Profit in $10,0000s',fontsize=18)
+    plt.xlabel('Population of City in 10,000s',fontsize=18)
+
+    return None
 
 # Compute cost function J(\theta)
 def computeCost(X,y,theta):
     "Compute cost function J(\theta)"
     numTrainEx = y.shape[0]
+    if (numTrainEx == 0):
+        raise InsufficientTrainingExamples('numTrainEx = 0')
     diffVec = np.subtract(np.dot(X,theta),y)
     diffVecSq = np.multiply(diffVec,diffVec)
-    if (numTrainEx > 0):
-        jTheta = (np.sum(diffVecSq,axis=0))/(2*numTrainEx)
-    else:
-        jTheta = 0
+    jTheta = (np.sum(diffVecSq,axis=0))/(2*numTrainEx)
+
     return(np.asscalar(jTheta))
 
 # Run gradient descent
 def gradientDescent(X,y,theta,alpha,numiters):
     "Run gradient descent"
+    if (numiters <= 0):
+        raise InsufficientIterations('numiters <= 0')
     numTrainEx = y.shape[0]
+    if (numTrainEx == 0):
+        raise InsufficientTrainingExamples('numTrainEx = 0')
     jThetaArray = np.zeros((numiters,1))
-    if (numTrainEx > 0):
-        if (numiters >= 1):
-            for thetaIndex in range(0,numiters):
-                diffVec = np.subtract(np.dot(X,theta),y)
-                diffVecTimesX = np.c_[np.multiply(diffVec,np.reshape(X[:,0],(numTrainEx,1))),np.multiply(diffVec,np.reshape(X[:,1],(numTrainEx,1)))]
-                thetaNew = np.subtract(theta,alpha*(1/numTrainEx)*np.reshape(np.sum(diffVecTimesX,axis=0),(2,1)))
-                jThetaArray[thetaIndex] = computeCost(X,y,thetaNew)
-                theta = thetaNew
-        else:
-            theta = np.zeros((2,1))
-    else:
-        theta = np.zeros((2,1))
+    for thetaIndex in range(0,numiters):
+        diffVec = np.subtract(np.dot(X,theta),y)
+        diffVecTimesX = np.c_[np.multiply(diffVec,np.reshape(X[:,0],(numTrainEx,1))),np.multiply(diffVec,np.reshape(X[:,1],(numTrainEx,1)))]
+        thetaNew = np.subtract(theta,alpha*(1/numTrainEx)*np.reshape(np.sum(diffVecTimesX,axis=0),(2,1)))
+        jThetaArray[thetaIndex] = computeCost(X,y,thetaNew)
+        theta = thetaNew
+
     return(theta)
 
 # Main function
 def main():
     "Main function"
+    print("Plotting Data ...")
     foodTruckData = np.genfromtxt("../foodTruckData.txt",delimiter=",")
+    returnCode = plotData(foodTruckData[:,0],foodTruckData[:,1])
+    plt.show()
+    input("Program paused. Press enter to continue.")
+    print("")
+    print("Running Gradient Descent ...")
     numTrainEx = foodTruckData.shape[0]
     onesVec = np.ones((numTrainEx,1))
     xMat = np.c_[onesVec,foodTruckData[:,0]]
@@ -47,23 +77,55 @@ def main():
 
     # Compute initial cost
     initCost = computeCost(xMat,yVec,thetaVec)
-    print("initial cost = %f" % initCost)
+    print("ans = %.3f" % initCost)
 
     # Run gradient descent
     iterations = 1500
     alpha = 0.01
     thetaFinal = gradientDescent(xMat,yVec,thetaVec,alpha,iterations)
-    print("gradient descent returns theta = %s" % np.array_str(thetaFinal))
+    print("Theta found by gradient descent: %s" % np.array_str(np.transpose(np.round(thetaFinal,6))))
+    returnCode = plotData(foodTruckData[:,0],foodTruckData[:,1])
+    plt.hold(True)
+    plt.plot(xMat[:,1],np.dot(xMat,thetaFinal),'b-',markersize=18)
+    plt.legend(('Training data','Linear regression'),loc='lower right')
+    plt.hold(False)
+    plt.show()
 
     # Predict profit for population size of 35000
     predProfit1 = np.asscalar(np.dot(np.array([1,3.5]),thetaFinal))
     predProfit1Scaled = 10000*predProfit1
-    print("predicted profit (in dollars) for population size of 35000 = %f" % predProfit1Scaled)
+    print("For population = 35,000, we predict a profit of %.6f" % predProfit1Scaled)
 
     # Predict profit for population size of 70000
     predProfit2 = np.asscalar(np.dot(np.array([1,7.0]),thetaFinal))
     predProfit2Scaled = 10000*predProfit2
-    print("predicted profit (in dollars) for population size of 70000 = %f" % predProfit2Scaled)
+    print("For population = 70,000, we predict a profit of %.6f" % predProfit2Scaled)
+    input("Program paused. Press enter to continue.")
+    print("")
+    print("Visualizing J(theta_0, theta_1) ...\n")
+    theta0Vals = np.linspace(-10,10,num=100)
+    theta1Vals = np.linspace(-1,4,num=100)
+    jVals = np.zeros((theta0Vals.shape[0],theta1Vals.shape[0]))
+    for theta0Index in range(0,theta0Vals.shape[0]):
+        for theta1Index in range(0,theta1Vals.shape[0]):
+            tVec = np.vstack((theta0Vals[theta0Index],theta1Vals[theta1Index]))
+            jVals[theta0Index,theta1Index] = computeCost(xMat,yVec,tVec);
+    jValsTrans = np.transpose(jVals)
+    theta0ValsX,theta1ValsY = np.meshgrid(theta0Vals,theta1Vals)
+    jValsReshape = jValsTrans.reshape(theta0ValsX.shape)
+    fig = plt.figure()
+    ax = fig.add_subplot(111,projection='3d')
+    ax.plot_surface(theta0ValsX,theta1ValsY,jValsReshape,rstride=1,cstride=1,cmap=cm.jet)
+    ax.set_ylabel(r'$\Theta_1$',fontsize=18)
+    ax.set_xlabel(r'$\Theta_0$',fontsize=18)
+    plt.show()
+    plt.contour(theta0ValsX,theta1ValsY,jValsReshape,levels=np.logspace(-2,3,20))
+    plt.ylabel(r'$\Theta_1$',fontsize=18)
+    plt.xlabel(r'$\Theta_0$',fontsize=18)
+    plt.hold(True)
+    plt.plot(thetaFinal[0],thetaFinal[1],'rx',markersize=18,markeredgewidth=3)
+    plt.hold(False)
+    plt.show()
 
 # Call main function
 if __name__ == "__main__":
--- programming_exercise_1/Python/ex1_multi.py
+++ programming_exercise_1/Python/ex1_multi.py
@@ -2,112 +2,145 @@
 # Programming Exercise 1: Linear Regression
 # Problem: Predict housing prices given sizes/bedrooms of various houses
 # Linear regression with multiple variables
+import matplotlib.pyplot as plt
 import numpy as np
+
+class InsufficientFeatures(Exception):
+    def __init__(self,value):
+        self.value = value
+    def __str__(self):
+        return repr(self.value)
+
+class InsufficientIterations(Exception):
+    def __init__(self,value):
+        self.value = value
+    def __str__(self):
+        return repr(self.value)
+
+class InsufficientTrainingExamples(Exception):
+    def __init__(self,value):
+        self.value = value
+    def __str__(self):
+        return repr(self.value)
 
 # Perform feature normalization
 def featureNormalize(X):
     "Perform feature normalization"
     numTrainEx = X.shape[0]
+    if (numTrainEx == 0):
+        raise InsufficientTrainingExamples('numTrainEx = 0')
     numFeatures = X.shape[1]
+    if (numFeatures == 0):
+        raise InsufficientFeatures('numFeatures = 0')
     xNormalized = np.zeros((numTrainEx,numFeatures))
     muVec = np.mean(X,axis=0)
-    if (numFeatures >= 1):
-        sigmaVec = np.std(X,axis=0)
-        if (numTrainEx >= 1):
-            for index in range(0,numTrainEx):
-                xNormalized[index] = np.divide(np.subtract(X[index,:],muVec),sigmaVec)
-        else:
-            xNormalized = 0
-            muVec = np.zeros((1,2))
-            sigmaVec = np.ones((1,2))
-    else:
-        xNormalized = np.zeros((numTrainEx,0))
-        muVec = np.zeros((1,2))
-        sigmaVec = np.ones((1,2))
+    sigmaVec = np.std(X,axis=0)
+    for index in range(0,numTrainEx):
+        xNormalized[index] = np.divide(np.subtract(X[index,:],muVec),sigmaVec)
     returnList = {'xNormalized': xNormalized,'muVec': muVec,'sigmaVec': sigmaVec}
+
     return(returnList)
 
 # Compute cost function J(\theta)
 def computeCostMulti(X,y,theta):
     "Compute cost function J(\theta)"
     numTrainEx = y.shape[0]
+    if (numTrainEx == 0):
+        raise InsufficientTrainingExamples('numTrainEx = 0')
     diffVec = np.subtract(np.dot(X,theta),y)
     diffVecSq = np.multiply(diffVec,diffVec)
-    if (numTrainEx > 0):
-        jTheta = (np.sum(diffVecSq,axis=0))/(2*numTrainEx)
-    else:
-        jTheta = 0
+    jTheta = (np.sum(diffVecSq,axis=0))/(2*numTrainEx)
+
     return(np.asscalar(jTheta))
 
 # Run gradient descent
 def gradientDescentMulti(X,y,theta,alpha,numiters):
     "Run gradient descent"
+    if (numiters <= 0):
+        raise InsufficientIterations('numiters <= 0')
     numTrainEx = y.shape[0]
+    if (numTrainEx == 0):
+        raise InsufficientTrainingExamples('numTrainEx = 0')
     numFeatures = X.shape[1]
+    if (numFeatures == 0):
+        raise InsufficientFeatures('numFeatures = 0')
     jThetaArray = np.zeros((numiters,1))
-    if (numTrainEx > 0):
-        if (numFeatures >= 2):
-            if (numiters >= 1):
-                for thetaIndex in range(0,numiters):
-                    diffVec = np.subtract(np.dot(X,theta),y)
-                    diffVecTimesX = np.multiply(diffVec,np.reshape(X[:,0],(numTrainEx,1)))
-                    for featureIndex in range(1,numFeatures):
-                        diffVecTimesX = np.c_[diffVecTimesX,np.multiply(diffVec,np.reshape(X[:,featureIndex],(numTrainEx,1)))]
-                    thetaNew = np.subtract(theta,alpha*(1/numTrainEx)*np.reshape(np.sum(diffVecTimesX,axis=0),(numFeatures,1)))
-                    jThetaArray[thetaIndex] = computeCostMulti(X,y,thetaNew)
-                    theta = thetaNew
-            else:
-                theta = np.zeros((3,1))
-        else:
-            theta = np.zeros((3,1))
-    else:
-        theta = np.zeros((3,1))
-    return(theta)
+    for thetaIndex in range(0,numiters):
+        diffVec = np.subtract(np.dot(X,theta),y)
+        diffVecTimesX = np.multiply(diffVec,np.reshape(X[:,0],(numTrainEx,1)))
+        for featureIndex in range(1,numFeatures):
+            diffVecTimesX = np.c_[diffVecTimesX,np.multiply(diffVec,np.reshape(X[:,featureIndex],(numTrainEx,1)))]
+        thetaNew = np.subtract(theta,alpha*(1/numTrainEx)*np.reshape(np.sum(diffVecTimesX,axis=0),(numFeatures,1)))
+        jThetaArray[thetaIndex] = computeCostMulti(X,y,thetaNew)
+        theta = thetaNew
+    returnList = {'theta': theta,'jHistory': jThetaArray}
+
+    return(returnList)
 
 # Compute normal equations
 def normalEqn(X,y):
     "Compute normal equations"
     theta = np.dot(np.dot(np.linalg.pinv(np.dot(np.transpose(X),X)),np.transpose(X)),y)
+
     return(theta)
 
 # Main function
 def main():
     "Main function"
+    print("Loading data ...")
     housingData = np.genfromtxt("../housingData.txt",delimiter=",")
     xMat = np.c_[housingData[:,0],housingData[:,1]]
+    numTrainEx = housingData.shape[0]
+    yVec = np.reshape(housingData[:,2],(numTrainEx,1))
+    print("First 10 examples from the dataset:")
+    for trainingExIndex in range(0,10):
+        print(" x = %s, y = %s" % (np.array_str(xMat[trainingExIndex,:].astype(int)),np.array_str(yVec[trainingExIndex,:].astype(int))))
+    input("Program paused. Press enter to continue.")
+    print("")
 
     # Perform feature normalization
+    print("Normalizing Features ...")
     featureNormalizeList = featureNormalize(xMat)
     xMatNormalized = featureNormalizeList['xNormalized']
     muVec = featureNormalizeList['muVec']
     sigmaVec = featureNormalizeList['sigmaVec']
-    numTrainEx = housingData.shape[0]
     onesVec = np.ones((numTrainEx,1))
     xMatAug = np.c_[onesVec,xMat]
     xMatNormalizedAug = np.c_[onesVec,xMatNormalized]
-    yVec = np.reshape(housingData[:,2],(numTrainEx,1))
     thetaVec = np.zeros((3,1))
     iterations = 400
     alpha = 0.1
 
     # Run gradient descent
-    thetaFinal = gradientDescentMulti(xMatNormalizedAug,yVec,thetaVec,alpha,iterations)
-    print("gradient descent returns theta = %s" % np.array_str(thetaFinal))
+    print("Running gradient descent ...")
+    gradientDescentMultiList = gradientDescentMulti(xMatNormalizedAug,yVec,thetaVec,alpha,iterations)
+    thetaFinal = gradientDescentMultiList['theta']
+    jHistory = gradientDescentMultiList['jHistory']
+    plt.plot(np.arange(jHistory.shape[0]),jHistory,'b-',markersize=18)
+    plt.ylabel('Cost J',fontsize=18)
+    plt.xlabel('Number of iterations',fontsize=18)
+    plt.show()
+    print("Theta computed from gradient descent:")
+    print("%s\n" % np.array_str(np.round(thetaFinal,6)))
 
     # Predict price for a 1650 square-foot house with 3 bedrooms
     xMatNormalized1 = np.reshape(np.divide(np.subtract(np.array([1650,3]),muVec),sigmaVec),(1,2))
     xMatNormalized1Aug = np.c_[np.ones((1,1)),xMatNormalized1]
     predPrice1 = np.asscalar(np.dot(xMatNormalized1Aug,thetaFinal))
-    print("predicted price (in dollars) for 1650 square-foot house with 3 bedrooms = %f" % predPrice1)
+    print("Predicted price of a 1650 sq-ft, 3 br house (using gradient descent):\n $%.6f" % predPrice1)
+    input("Program paused. Press enter to continue.")
+    print("")
 
     # Solve normal equations
+    print("Solving with normal equations...")
     thetaNormal = normalEqn(xMatAug,yVec)
-    print("normal equations return theta = %s" % np.array_str(thetaNormal))
+    print("Theta computed from the normal equations:")
+    print("%s\n" % np.array_str(np.round(thetaNormal,6)))
 
     # Use normal equations to predict price for a 1650 square-foot house with 3 bedrooms
     xMat2 = np.array([1,1650,3])
     predPrice2 = np.asscalar(np.dot(xMat2,thetaNormal))
-    print("predicted price (in dollars) for 1650 square-foot house with 3 bedrooms using normal equations = %f" % predPrice2)
+    print("Predicted price of a 1650 sq-ft, 3 br house (using normal equations):\n $%.6f" % predPrice2)
 
 # Call main function
 if __name__ == "__main__":
