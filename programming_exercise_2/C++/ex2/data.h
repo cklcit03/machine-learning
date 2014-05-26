ee6ae4e68a6d6c405354b63aea9d2474d37786f2
cklcit03 <flashbuzzer@gmail.com>
2014-05-26 16:03:34 -0400

    minor edit

--- programming_exercise_2/C++/ex2/data.h
+++ programming_exercise_2/C++/ex2/data.h
@@ -52,7 +52,7 @@ class Data
     num_features_ = training_data.n_cols-1;
     training_features_ = \
       arma::join_horiz(arma::ones<arma::vec>(num_train_ex_),\
-	  training_data.cols(0,num_features_-1));
+        training_data.cols(0,num_features_-1));
     training_labels_ = training_data.col(num_features_);
   }
 
