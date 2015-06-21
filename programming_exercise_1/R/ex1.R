428d00d9fd31fed5e3cfe76f449ae02256ea5a6f
cklcit03 <flashbuzzer@gmail.com>
2015-06-20 21:41:42 -0700

    conform to Google's R style guide

--- programming_exercise_1/R/ex1.R
+++ programming_exercise_1/R/ex1.R
@@ -15,112 +15,156 @@
 
 # Machine Learning
 # Programming Exercise 1: Linear Regression
-# Problem: Predict profits for a food truck given data for profits/populations of various cities
+# Problem: Predict profits for a food truck given data for profits/populations
+# of various cities
 # Linear regression with one variable
 
 # Load packages
 library(fields)
 library(pracma)
 
-# Plot data
-plotData <- function(x,y){
-  plot(x,y,col="red",pch="x",xlab="Population of City in 10,000s",ylab="Profit in $10,000s")
+PlotData <- function(x, y) {
+  # Plots data.
+  #
+  # Args:
+  #   x: X-values of data to be plotted.
+  #   y: Y-values of data to be plotted.
+  #
+  # Returns:
+  #   None.
+  plot(x, y, col="red", pch="x", xlab="Population of City in 10,000s",
+       ylab="Profit in $10,000s")
   return(0)
 }
 
-# Read key press
-readKey <- function(){
+ReadKey <- function() {
+  # Reads key press.
+  #
+  # Args:
+  #   None.
+  #
+  # Returns:
+  #   None.
   cat("Program paused.  Press enter to continue.")
   line <- readline()
   return(0)
 }
 
-# Compute cost function J(\theta)
-computeCost <- function(X,y,theta){
-  numTrainEx = dim(y)[1]
-  diffVec = X %*% theta-y
-  diffVecSq = diffVec * diffVec
-  if (numTrainEx > 0)
-    jTheta = (colSums(diffVecSq))/(2*numTrainEx)
-  else
+ComputeCost <- function(X, y, theta) {
+  # Computes cost function J(\theta).
+  #
+  # Args:
+  #   X: Matrix of features.
+  #   y: Vector of labels.
+  #   theta: Vector of parameters for linear regression.
+  #
+  # Returns:
+  #   jTheta: Linear regression cost.
+  numTrainEx <- dim(y)[1]
+  if (numTrainEx > 0) {
+    diffVec <- X %*% theta - y
+    diffVecSq <- diffVec * diffVec
+    jTheta <- (colSums(diffVecSq)) / (2 * numTrainEx)
+  } else {
     stop('Insufficient training examples')
+  }
   return(jTheta)
 }
 
-# Run gradient descent
-gradientDescent <- function(X,y,theta,alpha,numiters){
-  numTrainEx = dim(y)[1]
-  jThetaArray = t(t(rep(numiters)))
+GradientDescent <- function(X, y, theta, alpha, numiters) {
+  # Runs gradient descent.
+  #
+  # Args:
+  #   X: Matrix of features.
+  #   y: Vector of labels.
+  #   theta: Vector of parameters for linear regression.
+  #   alpha: Learning rate for gradient descent.
+  #   numiters: Number of iterations for gradient descent.
+  #
+  # Returns:
+  #   theta: Updated vector of parameters for linear regression.
+  numTrainEx <- dim(y)[1]
   if (numTrainEx > 0) {
     if (numiters >= 1) {
-      for(thetaIndex in 1:numiters) {
-        diffVec = X %*% theta-y
-        diffVecTimesX = cbind(diffVec*X[,1],diffVec*X[,2])
-        thetaNew = theta-t(alpha %*% (1/numTrainEx) %*% t(colSums(diffVecTimesX)))
-        jThetaArray[thetaIndex] <- computeCost(X,y,thetaNew)
-        theta = thetaNew
+      jThetaArray <- t(t(rep(numiters)))
+      for (thetaIndex in 1:numiters) {
+        diffVec <- X %*% theta - y
+        diffVecTimesX <- cbind(diffVec * X[, 1], diffVec * X[, 2])
+        thetaNew <- theta - t(alpha %*% (1 / numTrainEx) %*% 
+                              t(colSums(diffVecTimesX)))
+        jThetaArray[thetaIndex] <- ComputeCost(X, y, thetaNew)
+        theta <- thetaNew
       }
-    }
-    else
+    } else {
       stop('Insufficient iterations')
-  }
-  else
+    }
+  } else {
     stop('Insufficient training examples')
+  }
   return(theta)
 }
 
-# Use setwd() to set working directory to directory that contains this source file
+# Use setwd() to set working directory to directory that contains this source
+# file
 # Load file into R
-foodTruckData = read.csv("../foodTruckData.txt",header=FALSE)
+foodTruckData <- read.csv("../foodTruckData.txt", header=FALSE)
 
 # Plot data
 print("Plotting data ...")
-returnCode <- plotData(foodTruckData[,"V1"],foodTruckData[,"V2"])
-returnCode <- readKey()
-numTrainEx = dim(foodTruckData)[1]
-onesVec = t(t(rep(1,numTrainEx)))
-xMat = cbind(onesVec,foodTruckData[,"V1"])
-thetaVec = t(t(rep(0,2)))
-yVec = cbind(foodTruckData[,"V2"])
+returnCode <- PlotData(foodTruckData[, "V1"], foodTruckData[, "V2"])
+returnCode <- ReadKey()
+numTrainEx <- dim(foodTruckData)[1]
+onesVec <- t(t(rep(1, numTrainEx)))
+xMat <- cbind(onesVec, foodTruckData[, "V1"])
+thetaVec <- t(t(rep(0, 2)))
+yVec <- cbind(foodTruckData[, "V2"])
 
 # Compute initial cost
 print("Running Gradient Descent ...")
-initCost <- computeCost(xMat,yVec,thetaVec)
-print(sprintf("ans = %.3f",initCost))
+initCost <- ComputeCost(xMat, yVec, thetaVec)
+print(sprintf("ans = %.3f", initCost))
 
 # Run gradient descent
-iterations = 1500
-alpha = 0.01
-thetaFinal <- gradientDescent(xMat,yVec,thetaVec,alpha,iterations)
-print(sprintf("Theta found by gradient descent: %s",paste(format(round(thetaFinal,6),nsmall=6),collapse=" ")))
-lines(foodTruckData[,"V1"],xMat%*%thetaFinal,col="blue",pch="-")
-plotLegend <- legend('bottomright',col=c("red","blue"),pch=c("x","-"),legend=c("",""),bty="n",trace=TRUE)
-text(plotLegend$text$x-1,plotLegend$text$y,c('Training data','Linear regression'),pos=2)
+kIterations <- 1500
+kAlpha <- 0.01
+thetaFinal <- GradientDescent(xMat, yVec, thetaVec, kAlpha, kIterations)
+print(sprintf("Theta found by gradient descent: %s",
+              paste(format(round(thetaFinal, 6), nsmall=6), collapse=" ")))
+lines(foodTruckData[, "V1"], xMat %*% thetaFinal, col="blue", pch="-")
+plotLegend <- legend('bottomright', col=c("red", "blue"), pch=c("x", "-"),
+                     legend=c("", ""), bty="n", trace=TRUE)
+text(plotLegend$text$x-1, plotLegend$text$y,
+     c('Training data', 'Linear regression'), pos=2)
 
 # Predict profit for population size of 35000
-predProfit1 = matrix(c(1,3.5),nrow=1,ncol=2) %*% thetaFinal
-predProfit1Scaled = 10000*predProfit1
-print(sprintf("For population = 35,000, we predict a profit of %.6f",predProfit1Scaled))
+predProfit1 <- matrix(c(1, 3.5), nrow=1, ncol=2) %*% thetaFinal
+predProfit1Scaled <- 10000 * predProfit1
+print(sprintf("For population = 35,000, we predict a profit of %.6f",
+              predProfit1Scaled))
 
 # Predict profit for population size of 70000
-predProfit2 = matrix(c(1,7),nrow=1,ncol=2) %*% thetaFinal
-predProfit2Scaled = 10000*predProfit2
-print(sprintf("For population = 70,000, we predict a profit of %.6f",predProfit2Scaled))
-returnCode <- readKey()
+predProfit2 <- matrix(c(1, 7), nrow=1, ncol=2) %*% thetaFinal
+predProfit2Scaled <- 10000 * predProfit2
+print(sprintf("For population = 70,000, we predict a profit of %.6f",
+              predProfit2Scaled))
+returnCode <- ReadKey()
 
 # Surface plot
 print("Visualizing J(theta_0, theta_1) ...")
-theta0Vals = seq(-10,10,20/99)
-theta1Vals = seq(-1,4,5/99)
-J_vals = mat.or.vec(dim(cbind(theta0Vals))[1],dim(cbind(theta1Vals))[1])
-for(index0 in 1:dim(cbind(theta0Vals))[1]) {
-  for(index1 in 1:dim(cbind(theta1Vals))[1]){
-    t = rbind(theta0Vals[index0],theta1Vals[index1])
-    J_vals[index0,index1] <- computeCost(xMat,yVec,t)
+theta0Vals <- seq(-10, 10, 20/99)
+theta1Vals <- seq(-1, 4, 5/99)
+jVals <- mat.or.vec(dim(cbind(theta0Vals))[1], dim(cbind(theta1Vals))[1])
+for (index0 in 1:dim(cbind(theta0Vals))[1]) {
+  for (index1 in 1:dim(cbind(theta1Vals))[1]) {
+    t <- rbind(theta0Vals[index0], theta1Vals[index1])
+    jVals[index0, index1] <- ComputeCost(xMat, yVec, t)
   }
 }
-drape.plot(theta0Vals,theta1Vals,J_vals,xlab=expression(theta[0]),ylab=expression(theta[1]),zlab=expression(J_vals),horizontal=FALSE)
+drape.plot(theta0Vals, theta1Vals, jVals, xlab=expression(theta[0]),
+           ylab=expression(theta[1]), zlab=expression(jVals), horizontal=FALSE)
 
 # Contour plot
-contour(x=theta0Vals,y=theta1Vals,J_vals,levels=logspace(-2,3,20),col=tim.colors(12),drawlabels=FALSE,plot.title = title(xlab=expression(theta[0]),ylab=expression(theta[1])))
-points(x=thetaFinal[1,1],y=thetaFinal[2,1],col="red",cex=1.75,pch="x")
+contour(x=theta0Vals, y=theta1Vals, jVals, levels=logspace(-2, 3, 20),
+        col=tim.colors(12), drawlabels=FALSE,
+        plot.title=title(xlab=expression(theta[0]), ylab=expression(theta[1])))
+points(x=thetaFinal[1, 1], y=thetaFinal[2, 1], col="red", cex=1.75, pch="x")
--- programming_exercise_1/R/ex1_multi.R
+++ programming_exercise_1/R/ex1_multi.R
@@ -18,141 +18,191 @@
 # Problem: Predict housing prices given sizes/bedrooms of various houses
 # Linear regression with multiple variables
 
-# Load packages
-library(pracma)
-
-# Read key press
-readKey <- function(){
+ReadKey <- function() {
+  # Reads key press.
+  #
+  # Args:
+  #   None.
+  #
+  # Returns:
+  #   None.
   cat("Program paused.  Press enter to continue.")
   line <- readline()
   return(0)
 }
 
-# Perform feature normalization
-featureNormalize <- function(X){
-  numTrainEx = dim(X)[1]
-  numFeatures = dim(X)[2]
-  xNormalized = X
-  muVec = colMeans(X)
+FeatureNormalize <- function(X) {
+  # Performs feature normalization.
+  #
+  # Args:
+  #   X: Matrix of features.
+  #
+  # Returns:
+  #   returnList: List of three objects.
+  #               xNormalized: Matrix of normalized features.
+  #               muVec: Vector of mean values of features.
+  #               sigmaVec: Vector of standard deviations of features.
+  numTrainEx <- dim(X)[1]
+  numFeatures <- dim(X)[2]
   if (numFeatures >= 1) {
-    sigmaVec = t(rep(0,numFeatures))
-    for(index in 1:numFeatures) {
-      sigmaVec[index] <- sd(X[,index])
-    }
     if (numTrainEx >= 1) {
-      for(index in 1:numTrainEx) {
-        xNormalized[index,] = (X[index,]-muVec)/sigmaVec
+      xNormalized <- mat.or.vec(numTrainEx, numFeatures)
+      muVec <- colMeans(X)
+      sigmaVec <- t(rep(0, numFeatures))
+      for (index in 1:numFeatures) {
+        sigmaVec[index] <- sd(X[, index])
       }
-    }
-    else
+      for (index in 1:numTrainEx) {
+        xNormalized[index, ] <- (X[index, ] - muVec) / sigmaVec
+      }
+    } else {
       stop('Insufficient training examples')
-  }
-  else
+    }
+  } else {
     stop('Insufficient features')
-  returnList = list("xNormalized"=xNormalized,"muVec"=muVec,"sigmaVec"=sigmaVec)
+  }
+  returnList <- list("xNormalized"=xNormalized, "muVec"=muVec,
+                    "sigmaVec"=sigmaVec)
   return(returnList)
 }
 
-# Compute cost function J(\theta)
-computeCostMulti <- function(X,y,theta){
-  numTrainEx = dim(y)[1]
-  diffVec = X %*% theta-y
-  diffVecSq = diffVec * diffVec
-  if (numTrainEx > 0)
-    jTheta = (colSums(diffVecSq))/(2*numTrainEx)
-  else
+ComputeCostMulti <- function(X, y, theta) {
+  # Computes cost function J(\theta).
+  #
+  # Args:
+  #   X: Matrix of features.
+  #   y: Vector of labels.
+  #   theta: Vector of parameters for linear regression.
+  #
+  # Returns:
+  #   jTheta: Linear regression cost.
+  numTrainEx <- dim(y)[1]
+  if (numTrainEx > 0) {
+    diffVec <- X %*% theta - y
+    diffVecSq <- diffVec * diffVec
+    jTheta <- (colSums(diffVecSq)) / (2 * numTrainEx)
+  } else {
     stop('Insufficient training examples')
+  }
   return(jTheta)
 }
 
 # Run gradient descent
-gradientDescentMulti <- function(X,y,theta,alpha,numiters){
-  numTrainEx = dim(y)[1]
-  numFeatures = dim(X)[2]
-  jThetaArray = t(t(rep(0,numiters)))
+GradientDescentMulti <- function(X, y, theta, alpha, numiters) {
+  # Runs gradient descent.
+  #
+  # Args:
+  #   X: Matrix of features.
+  #   y: Vector of labels.
+  #   theta: Vector of parameters for linear regression.
+  #   alpha: Learning rate for gradient descent.
+  #   numiters: Number of iterations for gradient descent.
+  #
+  # Returns:
+  #   returnList: List of three objects.
+  #               theta: Updated vector of parameters for linear regression.
+  #               jHistory: Vector of linear regression cost at each iteration.
+  numTrainEx <- dim(y)[1]
+  numFeatures <- dim(X)[2]
   if (numTrainEx > 0) {
     if (numFeatures >= 2) {
       if (numiters >= 1) {
-        for(thetaIndex in 1:numiters) {
-          diffVec = X %*% theta-y
-          diffVecTimesX = cbind(diffVec * X[,1])
-          for(featureIndex in 2:numFeatures) {
-            diffVecTimesX = cbind(diffVecTimesX,diffVec * X[,featureIndex])
+        jThetaArray <- t(t(rep(0, numiters)))
+        for (thetaIndex in 1:numiters) {
+          diffVec <- X %*% theta - y
+          diffVecTimesX <- cbind(diffVec * X[, 1])
+          for (featureIndex in 2:numFeatures) {
+            diffVecTimesX <- cbind(diffVecTimesX,diffVec * X[, featureIndex])
           }
-          thetaNew = theta-t(alpha %*% (1/numTrainEx) %*% t(colSums(diffVecTimesX)))
-          jThetaArray[thetaIndex] <- computeCostMulti(X,y,thetaNew)
-          theta = thetaNew
+          thetaNew <- theta - t(alpha %*% (1 / numTrainEx) %*% 
+                                t(colSums(diffVecTimesX)))
+          jThetaArray[thetaIndex] <- ComputeCostMulti(X, y, thetaNew)
+          theta <- thetaNew
         }
-      }
-      else
+      } else {
         stop('Insufficient iterations')
-    }
-    else
+      }
+    } else {
       stop('Insufficient features')
-  }
-  else
+    }
+  } else {
     stop('Insufficient training examples')
-  returnList = list("theta"=theta,"jHistory"=jThetaArray)
+  }
+  returnList <- list("theta"=theta, "jHistory"=jThetaArray)
   return(returnList)
 }
 
-# Compute normal equations
-normalEqn <- function(X,y){
-  theta = pinv(t(X) %*% X) %*% t(X) %*% y
+NormalEqn <- function(X, y) {
+  # Computes normal equations.
+  #
+  # Args:
+  #   X: Matrix of features.
+  #   y: Vector of labels.
+  #
+  # Returns:
+  #   theta: Closed-form solution to linear regression.
+  theta <- pinv(t(X) %*% X) %*% t(X) %*% y
   return(theta)
 }
 
-# Use setwd() to set working directory to directory that contains this source file
+# Use setwd() to set working directory to directory that contains this source 
+# file
 # Load file into R
 print("Loading data ...")
-housingData = read.csv("../housingData.txt",header=FALSE)
-xMat = cbind(housingData[,"V1"],housingData[,"V2"])
-yVec = cbind(housingData[,"V3"])
+housingData = read.csv("../housingData.txt", header=FALSE)
+xMat <- cbind(housingData[, "V1"], housingData[, "V2"])
+yVec <- cbind(housingData[, "V3"])
 print("First 10 examples from the dataset: ")
-for(trainExIndex in 1:10){
-  print(sprintf("x = [%s], y = %d",paste(xMat[trainExIndex,],collapse=" "),yVec[trainExIndex,]))
+for (trainExIndex in 1:10) {
+  print(sprintf("x = [%s], y = %d", paste(xMat[trainExIndex, ], collapse=" "),
+                yVec[trainExIndex, ]))
 }
-returnCode <- readKey()
+returnCode <- ReadKey()
 
 # Perform feature normalization
 print("Normalizing Features ...")
-featureNormalizeList <- featureNormalize(xMat)
-xMatNormalized = featureNormalizeList$xNormalized
-muVec = featureNormalizeList$muVec
-sigmaVec = featureNormalizeList$sigmaVec
-numTrainEx = dim(housingData)[1]
-onesVec = t(t(rep(1,numTrainEx)))
-xMatAug = cbind(onesVec,xMat)
-xMatNormalizedAug = cbind(onesVec,xMatNormalized)
-thetaVec = t(t(rep(0,3)))
-iterations = 400
-alpha = 0.1
+featureNormalizeList <- FeatureNormalize(xMat)
+xMatNormalized <- featureNormalizeList$xNormalized
+muVec <- featureNormalizeList$muVec
+sigmaVec <- featureNormalizeList$sigmaVec
+numTrainEx <- dim(housingData)[1]
+onesVec <- t(t(rep(1, numTrainEx)))
+xMatAug <- cbind(onesVec, xMat)
+xMatNormalizedAug <- cbind(onesVec, xMatNormalized)
+thetaVec <- t(t(rep(0, 3)))
+kIterations <- 400
+kAlpha <- 0.1
 
 # Run gradient descent
 print("Running gradient descent ...")
-gradientDescentMultiList <- gradientDescentMulti(xMatNormalizedAug,yVec,thetaVec,alpha,iterations)
-thetaFinal = gradientDescentMultiList$theta
-jHistory = gradientDescentMultiList$jHistory
-plot(1:iterations,jHistory,type="l",col="blue",lwd=2,xlab="Number of iterations",ylab="Cost J")
+gradientDescentMultiList <- GradientDescentMulti(xMatNormalizedAug, yVec,
+                                                 thetaVec, kAlpha, kIterations)
+thetaFinal <- gradientDescentMultiList$theta
+jHistory <- gradientDescentMultiList$jHistory
+plot(1:kIterations, jHistory, type="l", col="blue", lwd=2,
+     xlab="Number of iterations", ylab="Cost J")
 print(sprintf("Theta computed from gradient descent: "))
-cat(format(round(thetaFinal,6),nsmall=6),sep="\n")
+cat(format(round(thetaFinal, 6), nsmall=6), sep="\n")
 
 # Predict price for a 1650 square-foot house with 3 bedrooms
-xMatNormalized1 = (matrix(c(1650,3),nrow=1,ncol=2)-muVec)/sigmaVec
-xMatNormalized1Aug = cbind(1,xMatNormalized1)
-predPrice1 = xMatNormalized1Aug %*% thetaFinal
-print(sprintf("Predicted price of a 1650 sq-ft, 3 br house (using gradient descent):"))
-print(sprintf("$%.6f",predPrice1))
-returnCode <- readKey()
+xMatNormalized1 <- (matrix(c(1650, 3), nrow=1, ncol=2) - muVec) / sigmaVec
+xMatNormalized1Aug <- cbind(1, xMatNormalized1)
+predPrice1 <- xMatNormalized1Aug %*% thetaFinal
+print(sprintf("Predicted price of a 1650 sq-ft, 3 br house (using gradient 
+              descent):"))
+print(sprintf("$%.6f", predPrice1))
+returnCode <- ReadKey()
 
 # Solve normal equations
 print("Solving with normal equations...")
-thetaNormal <- normalEqn(xMatAug,yVec)
+thetaNormal <- NormalEqn(xMatAug, yVec)
 print(sprintf("Theta computed from the normal equations: "))
-cat(format(round(thetaNormal,6),nsmall=6),sep="\n")
+cat(format(round(thetaNormal, 6), nsmall=6), sep="\n")
 
-# Use normal equations to predict price for a 1650 square-foot house with 3 bedrooms
-xMat2 = matrix(c(1,1650,3),nrow=1,ncol=3)
-predPrice2 = xMat2 %*% thetaNormal
-print(sprintf("Predicted price of a 1650 sq-ft, 3 br house (using normal equations):"))
-print(sprintf("$%.6f",predPrice2))
+# Use normal equations to predict price for a 1650 square-foot house with 3 
+# bedrooms
+xMat2 <- matrix(c(1, 1650, 3), nrow=1, ncol=3)
+predPrice2 <- xMat2 %*% thetaNormal
+print(sprintf("Predicted price of a 1650 sq-ft, 3 br house (using normal 
+              equations):"))
+print(sprintf("$%.6f", predPrice2))
