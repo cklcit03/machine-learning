35d865d880a671302bd4ee95744f048bb4487569
cklcit03 <flashbuzzer@gmail.com>
2015-06-23 22:53:44 -0700

    conform to Google's R style guide

--- programming_exercise_2/R/ex2.R
+++ programming_exercise_2/R/ex2.R
@@ -15,129 +15,211 @@
 
 # Machine Learning
 # Programming Exercise 2: Logistic Regression
-# Problem: Predict chances of university admission for an applicant given data for 
-# admissions decisions and test scores of various applicants
+# Problem: Predict chances of university admission for an applicant given data
+# for admissions decisions and test scores of various applicants
 
-# Plot data
-plotData <- function(X,y){
-  positiveIndices = which(y == 1)
-  negativeIndices = which(y == 0)
-  positiveExamples = cbind(X[positiveIndices,])
-  negativeExamples = cbind(X[negativeIndices,])
-  plot(positiveExamples[,1],positiveExamples[,2],cex=1.75,pch="+",xlab="",ylab="",xlim=c(30,100),ylim=c(30,100))
-  points(x=negativeExamples[,1],y=negativeExamples[,2],col="yellow",bg="yellow",cex=1.75,pch=22,xlab="",ylab="")
+PlotData <- function(X, y) {
+  # Plots data.
+  #
+  # Args:
+  #   X: Features to be plotted.
+  #   y: Data labels.
+  #
+  # Returns:
+  #   None.
+  positiveIndices <- which(y == 1)
+  negativeIndices <- which(y == 0)
+  positiveExamples <- cbind(X[positiveIndices, ])
+  negativeExamples <- cbind(X[negativeIndices, ])
+  plot(positiveExamples[, 1], positiveExamples[, 2], cex=1.75, pch="+",
+       xlab="", ylab="", xlim=c(30, 100), ylim=c(30, 100))
+  points(x=negativeExamples[, 1], y=negativeExamples[, 2], col="yellow",
+         bg="yellow", cex=1.75, pch=22, xlab="", ylab="")
   return(0)
 }
 
-# Plot decision boundary
-plotDecisionBoundary <- function(X,y,theta){
-  returnCode <- plotData(cbind(X[,2],X[,3]),y)
-  yLineVals = (theta[1]+theta[2]*X[,2])/(-1*theta[3])
-  lines(cbind(X[,2]),yLineVals,col="blue",pch="-")
+PlotDecisionBoundary <- function(X, y, theta) {
+  # Plots decision boundary.
+  #
+  # Args:
+  #   X: Features that have already been plotted.
+  #   y: Data labels.
+  #   theta: Parameter that determines slope of decision boundary.
+  #
+  # Returns:
+  #   None.
+  returnCode <- PlotData(cbind(X[, 2], X[, 3]), y)
+  yLineVals <- (theta[1] + theta[2] * X[, 2]) / (-1 * theta[3])
+  lines(cbind(X[, 2]), yLineVals, col="blue", pch="-")
   return(0)
 }
 
-# Read key press
-readKey <- function(){
-  cat("Program paused. Press enter to continue.")
+ReadKey <- function() {
+  # Reads key press.
+  #
+  # Args:
+  #   None.
+  #
+  # Returns:
+  #   None.
+  cat("Program paused.  Press enter to continue.")
   line <- readline()
   return(0)
 }
 
-# Compute sigmoid function
-computeSigmoid <- function(z){
-  sigmoidZ = 1/(1+exp(-z))
+ComputeSigmoid <- function(z) {
+  # Computes sigmoid function.
+  #
+  # Args:
+  #   z: Can be a scalar, a vector or a matrix.
+  #
+  # Returns:
+  #   None.
+  sigmoidZ <- 1 / (1 + exp(-z))
   return(sigmoidZ)
 }
 
-# Compute cost function J(\theta)
-computeCost <- function(theta,X,y,numTrainEx){
-  hTheta <- computeSigmoid(X%*%theta)
-  if (numTrainEx > 0)
-    jTheta = (colSums(-y*log(hTheta)-(1-y)*log(1-hTheta)))/numTrainEx
-  else
+ComputeCost <- function(theta, X, y, numTrainEx) {
+  # Computes cost function J(\theta).
+  #
+  # Args:
+  #   theta: Vector of parameters for logistic regression.
+  #   X: Matrix of features.
+  #   y: Vector of labels.
+  #   numTrainEx: Number of training examples.
+  #
+  # Returns:
+  #   jTheta: Logistic regression cost.
+  if (numTrainEx > 0) {
+    hTheta <- ComputeSigmoid(X %*% theta)
+    jTheta <- (colSums(-y * log(hTheta) 
+                       - (1 - y) * log(1 - hTheta))) / numTrainEx
+  } else {
     stop('Insufficient training examples')
+  }
   return(jTheta)  
 }
 
-# Compute gradient of cost function J(\theta)
-computeGradient <- function(theta,X,y,numTrainEx){
-  numFeatures = dim(X)[2]
-  hTheta <- computeSigmoid(X%*%theta)
+ComputeGradient <- function(theta, X, y, numTrainEx) {
+  # Computes gradient of cost function J(\theta).
+  #
+  # Args:
+  #   theta: Vector of parameters for logistic regression.
+  #   X: Matrix of features.
+  #   y: Vector of labels.
+  #   numTrainEx: Number of training examples.
+  #
+  # Returns:
+  #   gradArray: Vector of logistic regression gradients (one per feature).
+  numFeatures <- dim(X)[2]
   if (numFeatures > 0) {
     if (numTrainEx > 0) {
-      gradArray = matrix(0,numFeatures,1)
-      gradTermArray = matrix(0,numTrainEx,numFeatures)
-      for(gradIndex in 1:numFeatures) {
-        gradTermArray[,gradIndex] = (hTheta-y)*X[,gradIndex]
-        gradArray[gradIndex] = (sum(gradTermArray[,gradIndex]))/(numTrainEx)
+      hTheta <- ComputeSigmoid(X %*% theta)
+      gradArray <- matrix(0, numFeatures, 1)
+      gradTermArray <- matrix(0, numTrainEx, numFeatures)
+      for (gradIndex in 1:numFeatures) {
+        gradTermArray[, gradIndex] <- (hTheta - y) * X[, gradIndex]
+        gradArray[gradIndex] <- 
+          (sum(gradTermArray[, gradIndex])) / (numTrainEx)
       }
-    }
-    else
+    } else {
       stop('Insufficient training examples')
-  }
-  else
+    }
+  } else {
     stop('Insufficient features')
+  }
   return(gradArray)
 }
 
-# Aggregate computed cost and gradient
-computeCostGradList <- function(X,y,theta){
-  numTrainEx = dim(y)[1]
-  jTheta <- computeCost(theta,X,y,numTrainEx)
-  gradArray <- computeGradient(theta,X,y,numTrainEx)
-  returnList = list("jTheta"=jTheta,"gradArray"=gradArray)
+ComputeCostGradList <- function(X, y, theta) {
+  # Aggregates computed cost and gradient.
+  #
+  # Args:
+  #   X: Matrix of features.
+  #   y: Vector of labels.
+  #   theta: Vector of parameters for logistic regression.
+  #
+  # Returns:
+  #   returnList: List of two objects.
+  #               jTheta: Updated vector of parameters for logistic regression.
+  #               gradArray: Updated vector of logistic regression gradients 
+  #                          (one per feature).
+  numTrainEx <- dim(y)[1]
+  jTheta <- ComputeCost(theta, X, y, numTrainEx)
+  gradArray <- ComputeGradient(theta, X, y, numTrainEx)
+  returnList <- list("jTheta"=jTheta, "gradArray"=gradArray)
   return(returnList)
 }
 
-# Perform label prediction on training data
-labelPrediction <- function(X,theta){
-  sigmoidArr <- computeSigmoid(X%*%theta)
-  p = (sigmoidArr >= 0.5)
+LabelPrediction <- function(X, theta) {
+  # Performs label prediction on training data.
+  #
+  # Args:
+  #   X: Matrix of features.
+  #   theta: Vector of parameters for logistic regression.
+  #
+  # Returns:
+  #   p: Vector of predictions (one per example).
+  sigmoidArr <- ComputeSigmoid(X %*% theta)
+  p <- (sigmoidArr >= 0.5)
   return(p)
 }
 
-# Use setwd() to set working directory to directory that contains this source file
+# Use setwd() to set working directory to directory that contains this source
+# file
 # Load file into R
-applicantData = read.csv("../applicantData.txt",header=FALSE)
+applicantData <- read.csv("../applicantData.txt", header=FALSE)
 
 # Plot data
-print("Plotting data with + indicating (y = 1) examples and o indicating (y = 0) examples.")
-returnCode <- plotData(cbind(applicantData[,"V1"],applicantData[,"V2"]),applicantData[,"V3"])
-title(xlab="Exam 1 score",ylab="Exam 2 score")
-plotLegend <- legend('bottomright',col=c("black","yellow"),pt.bg=c("black","yellow"),pch=c(43,22),pt.cex=1.75,legend=c("",""),bty="n",trace=TRUE)
-text(plotLegend$text$x-3,plotLegend$text$y,c('Admitted','Not admitted'),pos=2)
-returnCode <- readKey()
-numTrainEx = dim(applicantData)[1]
-numFeatures = dim(applicantData)[2]-1
-onesVec = t(t(rep(1,numTrainEx)))
-xMat = cbind(onesVec,applicantData[,"V1"],applicantData[,"V2"])
-yVec = cbind(applicantData[,"V3"])
-thetaVec = t(t(rep(0,numFeatures+1)))
+print("Plotting data with + indicating (y = 1) examples and o indicating 
+      (y = 0) examples.")
+returnCode <- PlotData(cbind(applicantData[, "V1"], applicantData[, "V2"]),
+                       applicantData[, "V3"])
+title(xlab="Exam 1 score", ylab="Exam 2 score")
+plotLegend <- legend('bottomright', col=c("black", "yellow"), 
+                     pt.bg=c("black", "yellow"), pch=c(43, 22), pt.cex=1.75, 
+                     legend=c("", ""), bty="n", trace=TRUE)
+text(plotLegend$text$x - 3, plotLegend$text$y, c('Admitted', 'Not admitted'),
+     pos=2)
+returnCode <- ReadKey()
+numTrainEx <- dim(applicantData)[1]
+numFeatures <- dim(applicantData)[2] - 1
+onesVec <- t(t(rep(1, numTrainEx)))
+xMat <- cbind(onesVec, applicantData[, "V1"], applicantData[, "V2"])
+yVec <- cbind(applicantData[, "V3"])
+thetaVec <- t(t(rep(0, numFeatures + 1)))
 
 # Compute initial cost and gradient
-initComputeCostList <- computeCostGradList(xMat,yVec,thetaVec)
-print(sprintf("Cost at initial theta (zeros): %.6f",initComputeCostList$jTheta))
+initComputeCostList <- ComputeCostGradList(xMat, yVec, thetaVec)
+print(sprintf("Cost at initial theta (zeros): %.6f", 
+              initComputeCostList$jTheta))
 print(sprintf("Gradient at initial theta (zeros): "))
-cat(format(round(initComputeCostList$gradArray,6),nsmall=6),sep="\n")
-returnCode <- readKey()
+cat(format(round(initComputeCostList$gradArray, 6), nsmall=6), sep="\n")
+returnCode <- ReadKey()
 
 # Use optim to solve for optimum theta and cost
-optimResult <- optim(thetaVec,fn=computeCost,gr=computeGradient,xMat,yVec,numTrainEx,method="BFGS",control=list(maxit=400))
-print(sprintf("Cost at theta found by optim: %.6f",optimResult$value))
+optimResult <- optim(thetaVec, fn=ComputeCost, gr=ComputeGradient, xMat, yVec,
+                     numTrainEx, method="BFGS", control=list(maxit=400))
+print(sprintf("Cost at theta found by optim: %.6f", optimResult$value))
 print(sprintf("theta: "))
-cat(format(round(optimResult$par,6),nsmall=6),sep="\n")
-returnCode <- plotDecisionBoundary(xMat,yVec,optimResult$par)
-title(xlab="Exam 1 score",ylab="Exam 2 score")
-plotLegend <- legend('bottomright',col=c("black","yellow","blue"),pt.bg=c("black","yellow","blue"),pch=c(43,22,45),pt.cex=1.75,legend=c("","",""),bty="n",trace=TRUE)
-text(plotLegend$text$x-3,plotLegend$text$y,c('Admitted','Not admitted','Decision Boundary'),pos=2)
-returnCode <- readKey()
+cat(format(round(optimResult$par, 6), nsmall=6), sep="\n")
+returnCode <- PlotDecisionBoundary(xMat, yVec, optimResult$par)
+title(xlab="Exam 1 score", ylab="Exam 2 score")
+plotLegend <- legend('bottomright', col=c("black", "yellow", "blue"),
+                     pt.bg=c("black", "yellow", "blue"), pch=c(43, 22, 45),
+                     pt.cex=1.75, legend=c("", "", ""), bty="n", trace=TRUE)
+text(plotLegend$text$x - 3, plotLegend$text$y, 
+     c('Admitted', 'Not admitted', 'Decision Boundary'), pos=2)
+returnCode <- ReadKey()
 
-# Predict admission probability for a student with score 45 on exam 1 and score 85 on exam 2
-admissionProb <- computeSigmoid(cbind(1,45,85)%*%optimResult$par)
-print(sprintf("For a student with scores 45 and 85, we predict an admission probability of %.6f",admissionProb))
+# Predict admission probability for a student with score 45 on exam 1 and score
+# 85 on exam 2
+admissionProb <- ComputeSigmoid(cbind(1, 45, 85) %*% optimResult$par)
+print(sprintf("For a student with scores 45 and 85, we predict an admission 
+              probability of %.6f", admissionProb))
 
 # Compute accuracy on training set
-trainingPredict <- (labelPrediction(xMat,optimResult$par)+0)
-print(sprintf("Train Accuracy: %.6f",100*apply((trainingPredict == yVec),2,mean)))
-returnCode <- readKey()
+trainingPredict <- (LabelPrediction(xMat, optimResult$par) + 0)
+print(sprintf("Train Accuracy: %.6f",
+              100 * apply((trainingPredict == yVec), 2, mean)))
+returnCode <- ReadKey()
--- programming_exercise_2/R/ex2_reg.R
+++ programming_exercise_2/R/ex2_reg.R
@@ -18,142 +18,242 @@
 # Problem: Predict chances of acceptance for a microchip given data for 
 # acceptance decisions and test scores of various microchips
 
-# Plot data
-plotData <- function(X,y){
-  positiveIndices = which(y == 1)
-  negativeIndices = which(y == 0)
-  positiveExamples = cbind(X[positiveIndices,])
-  negativeExamples = cbind(X[negativeIndices,])
-  plot(positiveExamples[,1],positiveExamples[,2],cex=1.75,pch="+",xlab="",ylab="",xlim=c(-1,1.5),ylim=c(-1,1.5))
-  points(x=negativeExamples[,1],y=negativeExamples[,2],col="yellow",bg="yellow",cex=1.75,pch=22,xlab="",ylab="")
+PlotData <- function(X, y) {
+  # Plots data.
+  #
+  # Args:
+  #   X: Features to be plotted.
+  #   y: Data labels.
+  #
+  # Returns:
+  #   None.
+  positiveIndices <- which(y == 1)
+  negativeIndices <- which(y == 0)
+  positiveExamples <- cbind(X[positiveIndices, ])
+  negativeExamples <- cbind(X[negativeIndices, ])
+  plot(positiveExamples[, 1], positiveExamples[, 2], cex=1.75, pch="+",
+       xlab="", ylab="", xlim=c(-1, 1.5), ylim=c(-1, 1.5))
+  points(x=negativeExamples[, 1], y=negativeExamples[, 2], col="yellow",
+         bg="yellow", cex=1.75, pch=22, xlab="", ylab="")
   return(0)
 }
 
-# Plot decision boundary
-plotDecisionBoundary <- function(X,y,theta){
-  returnCode <- plotData(cbind(X[,1],X[,2]),y)
-  u = seq(-1,1.5,length=50)
-  v = seq(-1,1.5,length=50)
-  z = mat.or.vec(dim(cbind(u))[1],dim(cbind(v))[1])
-  for(index0 in 1:dim(cbind(u))[1]) {
-    for(index1 in 1:dim(cbind(v))[1]) {
-      z[index0,index1] <- mapFeature(u[index0],v[index1])%*%theta;
+PlotDecisionBoundary <- function(X, y, theta) {
+  # Plots decision boundary.
+  #
+  # Args:
+  #   X: Features that have already been plotted.
+  #   y: Data labels.
+  #   theta: Parameter that determines slope of decision boundary.
+  #
+  # Returns:
+  #   None.
+  returnCode <- PlotData(cbind(X[, 1], X[, 2]), y)
+  u <- seq(-1, 1.5, length=50)
+  v <- seq(-1, 1.5, length=50)
+  z <- mat.or.vec(dim(cbind(u))[1], dim(cbind(v))[1])
+  for (index0 in 1:dim(cbind(u))[1]) {
+    for (index1 in 1:dim(cbind(v))[1]) {
+      z[index0, index1] <- MapFeature(u[index0], v[index1]) %*% theta
     }
   }
-  contour(x=u,y=v,z,nlevels=1,zlim=range(0),col="blue",lwd=2,drawlabels=FALSE,add=TRUE)
+  contour(x=u, y=v, z, nlevels=1, zlim=range(0), col="blue", lwd=2, 
+          drawlabels=FALSE, add=TRUE)
   return(0)
 }
 
-# Read key press
-readKey <- function(){
+ReadKey <- function() {
+  # Reads key press.
+  #
+  # Args:
+  #   None.
+  #
+  # Returns:
+  #   None.
   cat("Program paused. Press enter to continue.")
   line <- readline()
   return(0)
 }
 
-# Add polynomial features to training data
-mapFeature <- function(X1,X2){
-  degree = 6
-  numTrainEx = dim(cbind(X1,X2))[1]
-  augXMat = matrix(1,numTrainEx,1)
-  for(degIndex1 in 1:degree) {
-    for(degIndex2 in 0:degIndex1) {
-      augXMat = cbind(augXMat,(X1^(degIndex1-degIndex2))*(X2^degIndex2))
+MapFeature <- function(X1, X2) {
+  # Adds polynomial features to training data.
+  #
+  # Args:
+  #   X1: Vector of values for feature 1.
+  #   X2: Vector of values for feature 2.
+  #
+  # Returns:
+  #   augXMat: Vector of mapped features.
+  numTrainEx <- dim(cbind(X1, X2))[1]
+  if (numTrainEx > 0) {
+    augXMat <- matrix(1, numTrainEx, 1)
+    kDegree <- 6
+    for (degIndex1 in 1:kDegree) {
+      for (degIndex2 in 0:degIndex1) {
+        augXMat <- cbind(augXMat, 
+                         (X1 ^ (degIndex1 - degIndex2)) * (X2 ^ degIndex2))
+      }
     }
+  } else {
+    stop('Insufficient training examples')
   }
   return(augXMat)
 }
 
-# Compute sigmoid function
-computeSigmoid <- function(z){
-  sigmoidZ = 1/(1+exp(-z))
+ComputeSigmoid <- function(z) {
+  # Computes sigmoid function.
+  #
+  # Args:
+  #   z: Can be a scalar, a vector or a matrix.
+  #
+  # Returns:
+  #   None.
+  sigmoidZ <- 1 / (1 + exp(-z))
   return(sigmoidZ)
 }
 
-# Compute regularized cost function J(\theta)
-computeCost <- function(theta,X,y,numTrainEx,lambda){
-  hTheta <- computeSigmoid(X%*%theta)
-  thetaSquared = theta^2
+ComputeCost <- function(theta, X, y, numTrainEx, lambda) {
+  # Computes regularized cost function J(\theta).
+  #
+  # Args:
+  #   theta: Vector of parameters for regularized logistic regression.
+  #   X: Matrix of features.
+  #   y: Vector of labels.
+  #   numTrainEx: Number of training examples.
+  #   lambda: Regularization parameter.
+  #
+  # Returns:
+  #   jThetaReg: Regularized logistic regression cost.
   if (numTrainEx > 0) {
-    jTheta = (colSums(-y*log(hTheta)-(1-y)*log(1-hTheta)))/numTrainEx
-    jThetaReg = jTheta+(lambda/(2*numTrainEx))*sum(thetaSquared[-1])
-  }
-  else
+    hTheta <- ComputeSigmoid(X %*% theta)
+    thetaSquared <- theta ^ 2
+    jTheta <- (colSums(-y * log(hTheta) 
+                       - (1 - y) * log(1 - hTheta))) / numTrainEx
+    jThetaReg <- jTheta + (lambda / (2 * numTrainEx)) * sum(thetaSquared[-1])
+  } else {
     stop('Insufficient training examples')
+  }
   return(jThetaReg)
 }
 
-# Compute gradient of regularized cost function J(\theta)
-computeGradient <- function(theta,X,y,numTrainEx,lambda){
-  numFeatures = dim(X)[2]
-  hTheta <- computeSigmoid(X%*%theta)
+ComputeGradient <- function(theta, X, y, numTrainEx, lambda) {
+  # Computes gradient of regularized cost function J(\theta).
+  #
+  # Args:
+  #   theta: Vector of parameters for regularized logistic regression.
+  #   X: Matrix of features.
+  #   y: Vector of labels.
+  #   numTrainEx: Number of training examples.
+  #   lambda: Regularization parameter.
+  #
+  # Returns:
+  #   gradArrayReg: Vector of regularized logistic regression gradients (one 
+  #                 per feature).
+  numFeatures <- dim(X)[2]
   if (numFeatures > 0) {
     if (numTrainEx > 0) {
-      gradArray = matrix(0,numFeatures,1)
-      gradArrayReg = matrix(0,numFeatures,1)
-      gradTermArray = matrix(0,numTrainEx,numFeatures)
-      for(gradIndex in 1:numFeatures) {
-        gradTermArray[,gradIndex] = (hTheta-y)*X[,gradIndex]
-        gradArray[gradIndex] = (sum(gradTermArray[,gradIndex]))/(numTrainEx)
-        gradArrayReg[gradIndex] = gradArray[gradIndex]+(lambda/numTrainEx)*theta[gradIndex]
+      hTheta <- ComputeSigmoid(X %*% theta)
+      gradArray <- matrix(0, numFeatures, 1)
+      gradArrayReg <- matrix(0, numFeatures, 1)
+      gradTermArray <- matrix(0, numTrainEx, numFeatures)
+      for (gradIndex in 1:numFeatures) {
+        gradTermArray[, gradIndex] <- (hTheta - y) * X[, gradIndex]
+        gradArray[gradIndex] <- 
+          (sum(gradTermArray[, gradIndex])) / (numTrainEx)
+        gradArrayReg[gradIndex] <- gradArray[gradIndex] + 
+          (lambda / numTrainEx) * theta[gradIndex]
       }
-      gradArrayReg[1] = gradArrayReg[1]-(lambda/numTrainEx)*theta[1]
+      gradArrayReg[1] <- gradArrayReg[1] - (lambda / numTrainEx) * theta[1]
+    } else {
+      stop('Insufficient training examples')
     }
-  }
-  else
+  } else {
     stop('Insufficient features')
+  }
   return(gradArrayReg)
 }
 
-# Aggregate computed cost and gradient
-computeCostGradList <- function(X,y,theta,lambda){
-  numTrainEx = dim(y)[1]
-  jThetaReg <- computeCost(theta,X,y,numTrainEx,lambda)
-  gradArrayReg <- computeGradient(theta,X,y,numTrainEx,lambda)
-  returnList = list("jThetaReg"=jThetaReg,"gradArrayReg"=gradArrayReg)
+ComputeCostGradList <- function(X, y, theta, lambda) {
+  # Aggregates computed cost and gradient.
+  #
+  # Args:
+  #   X: Matrix of features.
+  #   y: Vector of labels.
+  #   theta: Vector of parameters for regularized logistic regression.
+  #   lambda: Regularization parameter.
+  #
+  # Returns:
+  #   returnList: List of two objects.
+  #               jThetaReg: Updated vector of parameters for regularized 
+  #                          logistic regression.
+  #               gradArrayReg: Updated vector of regularized logistic 
+  #                             regression gradients (one per feature).
+  numTrainEx <- dim(y)[1]
+  jThetaReg <- ComputeCost(theta, X, y, numTrainEx, lambda)
+  gradArrayReg <- ComputeGradient(theta, X, y, numTrainEx, lambda)
+  returnList <- list("jThetaReg"=jThetaReg, "gradArrayReg"=gradArrayReg)
   return(returnList)
 }
 
-# Perform label prediction on training data
-labelPrediction <- function(X,theta){
-  sigmoidArr <- computeSigmoid(X%*%theta)
-  p = (sigmoidArr >= 0.5)
+LabelPrediction <- function(X, theta){
+  # Performs label prediction on training data.
+  #
+  # Args:
+  #   X: Matrix of features.
+  #   theta: Vector of parameters for regularized logistic regression.
+  #
+  # Returns:
+  #   p: Vector of predictions (one per example).
+  sigmoidArr <- ComputeSigmoid(X %*% theta)
+  p <- (sigmoidArr >= 0.5)
   return(p)
 }
 
-# Use setwd() to set working directory to directory that contains this source file
+# Use setwd() to set working directory to directory that contains this source 
+# file
 # Load file into R
-microChipData = read.csv("../microChipData.txt",header=FALSE)
+microChipData <- read.csv("../microChipData.txt", header=FALSE)
 
 # Plot data
-returnCode <- plotData(cbind(microChipData[,"V1"],microChipData[,"V2"]),microChipData[,"V3"])
-title(xlab="Microchip Test 1",ylab="Microchip Test 2")
-plotLegend <- legend('bottomright',col=c("black","yellow"),pt.bg=c("black","yellow"),pch=c(43,22),pt.cex=1.75,legend=c("",""),bty="n",trace=TRUE)
-text(plotLegend$text$x-0.1,plotLegend$text$y,c('y = 1','y = 0'),pos=2)
-numTrainEx = dim(microChipData)[1]
-numFeatures = dim(microChipData)[2]-1
-xMat = cbind(microChipData[,"V1"],microChipData[,"V2"])
-yVec = cbind(microChipData[,"V3"])
+returnCode <- PlotData(cbind(microChipData[, "V1"], microChipData[, "V2"]),
+                       microChipData[, "V3"])
+title(xlab="Microchip Test 1", ylab="Microchip Test 2")
+plotLegend <- legend('bottomright', col=c("black", "yellow"),
+                     pt.bg=c("black", "yellow"), pch=c(43, 22), pt.cex=1.75,
+                     legend=c("", ""), bty="n", trace=TRUE)
+text(plotLegend$text$x - 0.1, plotLegend$text$y, c('y = 1', 'y = 0'), pos=2)
+numTrainEx <- dim(microChipData)[1]
+numFeatures <- dim(microChipData)[2] - 1
+xMat <- cbind(microChipData[, "V1"], microChipData[, "V2"])
+yVec <- cbind(microChipData[, "V3"])
 
 # Add polynomial features to training data
-featureXMat <- mapFeature(xMat[,1],xMat[,2])
-thetaVec = t(t(rep(0,dim(featureXMat)[2])))
+featureXMat <- MapFeature(xMat[, 1], xMat[, 2])
+thetaVec <- t(t(rep(0, dim(featureXMat)[2])))
 
 # Compute initial cost and gradient
-lambda = 1
-initComputeCostList <- computeCostGradList(featureXMat,yVec,thetaVec,lambda)
-print(sprintf("Cost at initial theta (zeros): %.6f",initComputeCostList$jTheta))
-returnCode <- readKey()
+kLambda <- 1
+initComputeCostList <- ComputeCostGradList(featureXMat, yVec, thetaVec, 
+                                           kLambda)
+print(sprintf("Cost at initial theta (zeros): %.6f",
+              initComputeCostList$jTheta))
+returnCode <- ReadKey()
 
 # Use optim to solve for optimum theta and cost
-optimResult <- optim(thetaVec,fn=computeCost,gr=computeGradient,featureXMat,yVec,numTrainEx,lambda,method="BFGS",control=list(maxit=400))
+optimResult <- optim(thetaVec, fn=ComputeCost, gr=ComputeGradient, featureXMat,
+                     yVec, numTrainEx, kLambda, method="BFGS",
+                     control=list(maxit=400))
 
 # Plot decision boundary
-returnCode <- plotDecisionBoundary(xMat,yVec,optimResult$par)
-title(main=sprintf("lambda = %g",lambda),xlab="Microchip Test 1",ylab="Microchip Test 2")
-plotLegend <- legend('bottomright',col=c("black","yellow"),pt.bg=c("black","yellow"),pch=c(43,22),pt.cex=1.75,legend=c("",""),bty="n",trace=TRUE)
-text(plotLegend$text$x-0.1,plotLegend$text$y,c('y = 1','y = 0'),pos=2)
+returnCode <- PlotDecisionBoundary(xMat, yVec, optimResult$par)
+title(main=sprintf("lambda = %g", kLambda), xlab="Microchip Test 1",
+      ylab="Microchip Test 2")
+plotLegend <- legend('bottomright', col=c("black", "yellow"),
+                     pt.bg=c("black", "yellow"), pch=c(43, 22), pt.cex=1.75,
+                     legend=c("", ""), bty="n", trace=TRUE)
+text(plotLegend$text$x - 0.1, plotLegend$text$y, c('y = 1', 'y = 0'), pos=2)
 
 # Compute accuracy on training set
-trainingPredict <- (labelPrediction(featureXMat,optimResult$par)+0)
-print(sprintf("Train Accuracy: %.6f",100*apply((trainingPredict == yVec),2,mean)))
+trainingPredict <- (LabelPrediction(featureXMat, optimResult$par) + 0)
+print(sprintf("Train Accuracy: %.6f",
+              100 * apply((trainingPredict == yVec), 2, mean)))
