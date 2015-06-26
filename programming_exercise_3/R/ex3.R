a55d2589f8e333c6db241fd9bc270b2144ac2242
cklcit03 <flashbuzzer@gmail.com>
2015-06-25 22:42:36 -0700

    conform to Google's R style guide

--- programming_exercise_3/R/ex3.R
+++ programming_exercise_3/R/ex3.R
@@ -21,136 +21,217 @@
 # Load packages
 library(pracma)
 
-# Display 2D data in a grid
-displayData <- function(X){
-  exampleWidth = round(sqrt(dim(X)[2]))
-  numRows = dim(X)[1]
-  numCols = dim(X)[2]
-  exampleHeight = (numCols/exampleWidth)
-  displayRows = floor(sqrt(numRows))
-  displayCols = ceiling(numRows/displayRows)
-  pad = 1
-  displayArray = matrix(-1,pad+displayRows*(exampleHeight+pad),pad+displayCols*(exampleWidth+pad))
-  currEx = 1
-  for(rowIndex in 1:displayRows) {
-    for(colIndex in 1:displayCols) {
-      if (currEx > numRows) {
-        break
+DisplayData <- function(X) {
+  # Displays 2D data in a grid.
+  #
+  # Args:
+  #   X: Matrix of 2D data that will be displayed using image().
+  #
+  # Returns:
+  #   None.
+  numRows <- dim(X)[1]
+  numCols <- dim(X)[2]
+  if (numRows > 0) {
+    if (numCols > 0) {
+      exampleWidth <- round(sqrt(numCols))
+      exampleHeight <- numCols / exampleWidth
+      displayRows <- floor(sqrt(numRows))
+      displayCols <- ceiling(numRows / displayRows)
+      kPad <- 1
+      displayArray <- matrix(-1, kPad + displayRows * (exampleHeight + kPad),
+                             kPad + displayCols * (exampleWidth + kPad))
+      currEx <- 1
+      for (rowIndex in 1:displayRows) {
+        for (colIndex in 1:displayCols) {
+          if (currEx > numRows) {
+            break
+          }
+          maxVal <- max(abs(X[currEx, ]))
+          rowIdx <- kPad + (rowIndex - 1) * (exampleHeight + kPad) + 
+            1:exampleHeight
+          colIdx <- kPad + (colIndex - 1) * (exampleWidth + kPad) + 
+            1:exampleWidth
+          xReshape <- matrix(X[currEx, ], nrow=exampleHeight, byrow=FALSE)
+          displayArray[rowIdx, colIdx] <- (1 / maxVal) * 
+            as.numeric(t(xReshape[nrow(xReshape):1, ]))
+          currEx <- currEx + 1
+        }
+        if (currEx > numRows) {
+          break
+        }
       }
-      maxVal = max(abs(X[currEx,]))
-      rowIdx = pad+(rowIndex-1)*(exampleHeight+pad)+1:exampleHeight
-      colIdx = pad+(colIndex-1)*(exampleWidth+pad)+1:exampleWidth
-      xReshape = matrix(X[currEx,],nrow=exampleHeight,byrow=FALSE)
-      displayArray[rowIdx,colIdx] = (1/maxVal)*as.numeric(t(xReshape[nrow(xReshape):1,]))
-      currEx = currEx+1
+      image(displayArray, col=gray.colors(256), axes=FALSE, zlim=c(-1, 1))
+    } else {
+      stop('Insufficient number of columns')
     }
-    if (currEx > numRows) {
-      break
-    }
+  } else {
+    stop('Insufficient number of rows')
   }
-  image(displayArray,col=gray.colors(256),axes=FALSE,zlim=c(-1,1))
   return(0)
 }
 
-# Read key press
-readKey <- function(){
+ReadKey <- function(){
+  # Reads key press.
+  #
+  # Args:
+  #   None.
+  #
+  # Returns:
+  #   None.
   cat("Program paused. Press enter to continue.")
   line <- readline()
   return(0)
 }
 
-# Compute sigmoid function
-computeSigmoid <- function(z){
-  sigmoidZ = 1/(1+exp(-z))
+ComputeSigmoid <- function(z) {
+  # Computes sigmoid function.
+  #
+  # Args:
+  #   z: Can be a scalar, a vector or a matrix.
+  #
+  # Returns:
+  #   sigmoidZ: Sigmoid function value.
+  sigmoidZ <- 1 / (1 + exp(-z))
   return(sigmoidZ)
 }
 
-# Compute regularized cost function J(\theta)
-computeCost <- function(theta,X,y,numTrainEx,lambda){
-  hTheta <- computeSigmoid(X%*%theta)
-  thetaSquared = theta^2
+ComputeCost <- function(theta, X, y, numTrainEx, lambda) {
+  # Computes regularized cost function J(\theta).
+  #
+  # Args:
+  #   theta: Vector of parameters for regularized logistic regression.
+  #   X: Matrix of features.
+  #   y: Vector of labels.
+  #   numTrainEx: Number of training examples.
+  #   lambda: Regularization parameter.
+  #
+  # Returns:
+  #   jThetaReg: Regularized logistic regression cost.
   if (numTrainEx > 0) {
-    jTheta = (colSums(-y*log(hTheta)-(1-y)*log(1-hTheta)))/numTrainEx
-    jThetaReg = jTheta+(lambda/(2*numTrainEx))*sum(thetaSquared[-1])
-  }
-  else
+    hTheta <- ComputeSigmoid(X %*% theta)
+    thetaSquared <- theta ^ 2
+    jTheta <- (colSums(-y * log(hTheta) 
+                       - (1 - y) * log(1 - hTheta))) / numTrainEx
+    jThetaReg <- jTheta + (lambda / (2 * numTrainEx)) * sum(thetaSquared[-1])
+  } else {
     stop('Insufficient training examples')
+  }
   return(jThetaReg)
 }
 
-# Compute gradient of regularized cost function J(\theta)
-computeGradient <- function(theta,X,y,numTrainEx,lambda){
-  numFeatures = dim(X)[2]
-  hTheta <- computeSigmoid(X%*%theta)
-  gradArray = matrix(0,numFeatures,1)
-  gradArrayReg = matrix(0,numFeatures,1)
-  gradTermArray = matrix(0,numTrainEx,numFeatures)
+ComputeGradient <- function(theta, X, y, numTrainEx, lambda) {
+  # Computes gradient of regularized cost function J(\theta).
+  #
+  # Args:
+  #   theta: Vector of parameters for regularized logistic regression.
+  #   X: Matrix of features.
+  #   y: Vector of labels.
+  #   numTrainEx: Number of training examples.
+  #   lambda: Regularization parameter.
+  #
+  # Returns:
+  #   gradArrayReg: Vector of regularized logistic regression gradients (one 
+  #                 per feature).
+  numFeatures <- dim(X)[2]
   if (numFeatures > 0) {
     if (numTrainEx > 0) {
-      for(gradIndex in 1:numFeatures) {
-        gradTermArray[,gradIndex] = (hTheta-y)*X[,gradIndex]
-        gradArray[gradIndex] = (sum(gradTermArray[,gradIndex]))/(numTrainEx)
-        gradArrayReg[gradIndex] = gradArray[gradIndex]+(lambda/numTrainEx)*theta[gradIndex]
+      hTheta <- ComputeSigmoid(X %*% theta)
+      gradArray <- matrix(0, numFeatures, 1)
+      gradArrayReg <- matrix(0, numFeatures, 1)
+      gradTermArray <- matrix(0, numTrainEx, numFeatures)
+      for (gradIndex in 1:numFeatures) {
+        gradTermArray[, gradIndex] <- (hTheta - y) * X[, gradIndex]
+        gradArray[gradIndex] <- 
+          (sum(gradTermArray[, gradIndex])) / (numTrainEx)
+        gradArrayReg[gradIndex] <- gradArray[gradIndex] + 
+          (lambda / numTrainEx) * theta[gradIndex]
       }
-      gradArrayReg[1] = gradArrayReg[1]-(lambda/numTrainEx)*theta[1]
-    }
-    else
+      gradArrayReg[1] <- gradArrayReg[1] - (lambda / numTrainEx) * theta[1]
+    } else {
       stop('Insufficient training examples')
-  }
-  else
+    }
+  } else {
     stop('Insufficient features')
+  }
   return(gradArrayReg)
 }
 
-# Train multiple logistic regression classifiers
-oneVsAll <- function(X,y,numLabels,lambda){
-  numTrainEx = dim(X)[1]
-  numFeatures = dim(X)[2]
-  allTheta = matrix(0,numLabels,numFeatures+1)
-  onesVec = t(t(rep(1,numTrainEx)))
-  augX = cbind(onesVec,X)
-  for(labelIndex in 1:numLabels) {
-    thetaVec = t(t(rep(0,numFeatures+1)))
-    optimResult <- optim(thetaVec,fn=computeCost,gr=computeGradient,augX,as.numeric(yVec == labelIndex),numTrainEx,lambda,method="BFGS",control=list(maxit=400))
-    allTheta[labelIndex,] = optimResult$par
+OneVsAll <- function(X, y, numLabels, lambda) {
+  # Trains multiple logistic regression classifiers.
+  #
+  # Args:
+  #   X: Matrix of features.
+  #   y: Vector of labels.
+  #   numLabels: Number of classes.
+  #   lambda: Regularization parameter.
+  #
+  # Returns:
+  #   allTheta: Vector of regularized logistic regression parameters (one 
+  #                 per class).
+  if (numLabels > 0) {
+    numTrainEx <- dim(X)[1]
+    numFeatures <- dim(X)[2]
+    allTheta <- matrix(0, numLabels, numFeatures + 1)
+    onesVec <- t(t(rep(1, numTrainEx)))
+    augX <- cbind(onesVec, X)
+    for (labelIndex in 1:numLabels) {
+      thetaVec <- t(t(rep(0, numFeatures + 1)))
+      optimResult <- optim(thetaVec, fn=ComputeCost, gr=ComputeGradient, augX, 
+                           as.numeric(yVec == labelIndex), numTrainEx, lambda,
+                           method="BFGS", control=list(maxit=400))
+      allTheta[labelIndex, ] <- optimResult$par
+    }
+  } else {
+    stop('Insufficient number of classes')
   }
   return(allTheta)
 }
 
-# Perform label prediction on training data
-predictOneVsAll <- function(X,allTheta){
-  numTrainEx = dim(X)[1]
-  onesVec = t(t(rep(1,numTrainEx)))
-  augX = cbind(onesVec,X)
-  sigmoidArr <- computeSigmoid(augX%*%t(allTheta))
-  p = apply(sigmoidArr,1,which.max)
+PredictOneVsAll <- function(X, allTheta) {
+  # Performs label prediction on training data.
+  #
+  # Args:
+  #   X: Matrix of features.
+  #   allTheta: Vector of regularized logistic regression parameters (one 
+  #                 per class).
+  #
+  # Returns:
+  #   p: Vector of predicted class labels (one per example).
+  numTrainEx <- dim(X)[1]
+  onesVec <- t(t(rep(1, numTrainEx)))
+  augX <- cbind(onesVec, X)
+  sigmoidArr <- ComputeSigmoid(augX %*% t(allTheta))
+  p <- apply(sigmoidArr, 1, which.max)
   return(p)
 }
 
-# Use setwd() to set working directory to directory that contains this source file
+# Use setwd() to set working directory to directory that contains this source 
+# file
 # Load file into R
 print(sprintf("Loading and Visualizing Data ..."))
-digitData = read.csv("../digitData.txt",header=FALSE)
-numTrainEx = dim(digitData)[1]
-xMat = as.matrix(subset(digitData,select=-c(V401)))
-yVec = as.vector(subset(digitData,select=c(V401)))
+digitData <- read.csv("../digitData.txt", header=FALSE)
+numTrainEx <- dim(digitData)[1]
+xMat <- as.matrix(subset(digitData, select=-c(V401)))
+yVec <- as.vector(subset(digitData, select=c(V401)))
 
 # Randomly select 100 data points to display
-randIndices = randperm(numTrainEx,numTrainEx)
-xMatSel = subset(xMat,(rownames(xMat)) %in% randIndices[1])
-for(randIndex in 2:100) {
-  xMatSel = rbind(xMatSel,subset(xMat,(rownames(xMat)) %in% randIndices[randIndex]))
+randIndices <- randperm(numTrainEx, numTrainEx)
+xMatSel <- subset(xMat, (rownames(xMat)) %in% randIndices[1])
+for (randIndex in 2:100) {
+  xMatSel <- rbind(xMatSel, subset(xMat, (rownames(xMat)) %in% 
+                                   randIndices[randIndex]))
 }
-returnCode <- displayData(xMatSel)
-returnCode <- readKey()
+returnCode <- DisplayData(xMatSel)
+returnCode <- ReadKey()
 
 # Train one logistic regression classifier for each digit
 print(sprintf("Training One-vs-All Logistic Regression..."))
-lambda = 0.1
-numLabels = 10
-allTheta <- oneVsAll(xMat,yVec,numLabels,lambda)
-returnCode <- readKey()
+kLambda <- 0.1
+kNumLabels <- 10
+allTheta <- OneVsAll(xMat, yVec, kNumLabels, kLambda)
+returnCode <- ReadKey()
 
 # Perform one-versus-all classification using logistic regression
-trainingPredict <- (predictOneVsAll(xMat,allTheta))
-print(sprintf("Training Set Accuracy: %.6f",100*apply((trainingPredict == yVec),2,mean)))
+trainingPredict <- (PredictOneVsAll(xMat, allTheta))
+print(sprintf("Training Set Accuracy: %.6f", 
+              100 * apply((trainingPredict == yVec), 2, mean)))
--- programming_exercise_3/R/ex3_nn.R
+++ programming_exercise_3/R/ex3_nn.R
@@ -22,99 +22,144 @@
 # Load packages
 library(pracma)
 
-# Display 2D data in a grid
-displayData <- function(X){
-  exampleWidth = round(sqrt(dim(X)[2]))
-  numRows = dim(X)[1]
-  numCols = dim(X)[2]
-  exampleHeight = (numCols/exampleWidth)
-  displayRows = floor(sqrt(numRows))
-  displayCols = ceiling(numRows/displayRows)
-  pad = 1
-  displayArray = matrix(-1,pad+displayRows*(exampleHeight+pad),pad+displayCols*(exampleWidth+pad))
-  currEx = 1
-  for(rowIndex in 1:displayRows) {
-    for(colIndex in 1:displayCols) {
-      if (currEx > numRows) {
-        break
+DisplayData <- function(X) {
+  # Displays 2D data in a grid.
+  #
+  # Args:
+  #   X: Matrix of 2D data that will be displayed using image().
+  #
+  # Returns:
+  #   None.
+  numRows <- dim(X)[1]
+  numCols <- dim(X)[2]
+  if (numRows > 0) {
+    if (numCols > 0) {
+      exampleWidth <- round(sqrt(numCols))
+      exampleHeight <- numCols / exampleWidth
+      displayRows <- floor(sqrt(numRows))
+      displayCols <- ceiling(numRows / displayRows)
+      kPad <- 1
+      displayArray <- matrix(-1, kPad + displayRows * (exampleHeight + kPad),
+                             kPad + displayCols * (exampleWidth + kPad))
+      currEx <- 1
+      for (rowIndex in 1:displayRows) {
+        for (colIndex in 1:displayCols) {
+          if (currEx > numRows) {
+            break
+          }
+          maxVal <- max(abs(X[currEx, ]))
+          rowIdx <- kPad + (rowIndex - 1) * (exampleHeight + kPad) + 
+            1:exampleHeight
+          colIdx <- kPad + (colIndex - 1) * (exampleWidth + kPad) + 
+            1:exampleWidth
+          xReshape <- matrix(X[currEx, ], nrow=exampleHeight, byrow=FALSE)
+          displayArray[rowIdx, colIdx] <- (1 / maxVal) * 
+            as.numeric(t(xReshape[nrow(xReshape):1, ]))
+          currEx <- currEx + 1
+        }
+        if (currEx > numRows) {
+          break
+        }
       }
-      maxVal = max(abs(X[currEx,]))
-      rowIdx = pad+(rowIndex-1)*(exampleHeight+pad)+1:exampleHeight
-      colIdx = pad+(colIndex-1)*(exampleWidth+pad)+1:exampleWidth
-      xReshape = matrix(X[currEx,],nrow=exampleHeight,byrow=FALSE)
-      displayArray[rowIdx,colIdx] = (1/maxVal)*as.numeric(t(xReshape[nrow(xReshape):1,]) )
-      currEx = currEx+1
+      image(displayArray, col=gray.colors(256), axes=FALSE, zlim=c(-1, 1))
+    } else {
+      stop('Insufficient number of columns')
     }
-    if (currEx > numRows) {
-      break
-    }
+  } else {
+    stop('Insufficient number of rows')
   }
-  image(displayArray,col=gray.colors(256),axes=FALSE,zlim=c(-1,1))
   return(0)
 }
 
-# Read key press
-readKey <- function(){
+ReadKey <- function(){
+  # Reads key press.
+  #
+  # Args:
+  #   None.
+  #
+  # Returns:
+  #   None.
   cat("Program paused. Press enter to continue.")
   line <- readline()
   return(0)
 }
 
-# Compute sigmoid function
-computeSigmoid <- function(z){
-  sigmoidZ = 1/(1+exp(-z))
+ComputeSigmoid <- function(z) {
+  # Computes sigmoid function.
+  #
+  # Args:
+  #   z: Can be a scalar, a vector or a matrix.
+  #
+  # Returns:
+  #   sigmoidZ: Sigmoid function value.
+  sigmoidZ <- 1 / (1 + exp(-z))
   return(sigmoidZ)
 }
 
-# Perform label prediction on training data
-predict <- function(Theta1,Theta2,X){
-  numTrainEx = dim(X)[1]
-  onesVec = t(t(rep(1,numTrainEx)))
-  augX = cbind(onesVec,X)
-  hiddenLayerActivation <- computeSigmoid(augX%*%t(Theta1))
-  onesVecMod = t(t(rep(1,dim(hiddenLayerActivation)[1])))
-  hiddenLayerActivationMod = cbind(onesVecMod,hiddenLayerActivation)
-  outputLayerActivation <- computeSigmoid(hiddenLayerActivationMod%*%t(Theta2))
-  p = apply(outputLayerActivation,1,which.max)
+Predict <- function(Theta1, Theta2, X) {
+  # Performs label prediction on training data.
+  #
+  # Args:
+  #   Theta1: Matrix of neural network parameters (map from input layer to
+  #           hidden layer).
+  #   Theta2: Matrix of neural network parameters (map from hidden layer to
+  #           output layer).
+  #        X: Matrix of features. 
+  #
+  # Returns:
+  #   p: Vector of predicted class labels (one per example).
+  numTrainEx <- dim(X)[1]
+  onesVec <- t(t(rep(1, numTrainEx)))
+  augX <- cbind(onesVec, X)
+  hiddenLayerActivation <- ComputeSigmoid(augX %*% t(Theta1))
+  onesVecMod <- t(t(rep(1, dim(hiddenLayerActivation)[1])))
+  hiddenLayerActivationMod <- cbind(onesVecMod, hiddenLayerActivation)
+  outputLayerActivation <- 
+    ComputeSigmoid(hiddenLayerActivationMod %*% t(Theta2))
+  p <- apply(outputLayerActivation, 1, which.max)
   return(p)
 }
 
-# Use setwd() to set working directory to directory that contains this source file
+# Use setwd() to set working directory to directory that contains this source 
+# file
 # Load file into R
 print(sprintf("Loading and Visualizing Data ..."))
-digitData = read.csv("../digitData.txt",header=FALSE)
-numTrainEx = dim(digitData)[1]
-xMat = as.matrix(subset(digitData,select=-c(V401)))
-yVec = as.vector(subset(digitData,select=c(V401)))
+digitData <- read.csv("../digitData.txt", header=FALSE)
+numTrainEx <- dim(digitData)[1]
+xMat <- as.matrix(subset(digitData, select=-c(V401)))
+yVec <- as.vector(subset(digitData, select=c(V401)))
 
 # Randomly select 100 data points to display
-randIndices = randperm(numTrainEx,numTrainEx)
-xMatSel = subset(xMat,(rownames(xMat)) %in% randIndices[1])
-for(randIndex in 2:100) {
-  xMatSel = rbind(xMatSel,subset(xMat,(rownames(xMat)) %in% randIndices[randIndex]))
+randIndices <- randperm(numTrainEx, numTrainEx)
+xMatSel <- subset(xMat, (rownames(xMat)) %in% randIndices[1])
+for (randIndex in 2:100) {
+  xMatSel <- rbind(xMatSel, subset(xMat, (rownames(xMat)) %in% 
+                                   randIndices[randIndex]))
 }
-returnCode <- displayData(xMatSel)
-returnCode <- readKey()
+returnCode <- DisplayData(xMatSel)
+returnCode <- ReadKey()
 
 # Load two files that contain parameters trained by a neural network into R
 print(sprintf("Loading Saved Neural Network Parameters ..."))
-theta1 = read.csv("../Theta1.txt",header=FALSE)
-theta2 = read.csv("../Theta2.txt",header=FALSE)
-theta1Mat = as.matrix(theta1)
-theta2Mat = as.matrix(theta2)
+theta1 <- read.csv("../Theta1.txt", header=FALSE)
+theta2 <- read.csv("../Theta2.txt", header=FALSE)
+theta1Mat <- as.matrix(theta1)
+theta2Mat <- as.matrix(theta2)
 
 # Perform one-versus-all classification using trained parameters
-trainingPredict <- (predict(theta1Mat,theta2Mat,xMat))
-print(sprintf("Training Set Accuracy: %.6f",100*apply((trainingPredict == yVec),2,mean)))
-returnCode <- readKey()
+trainingPredict <- (Predict(theta1Mat, theta2Mat, xMat))
+print(sprintf("Training Set Accuracy: %.6f", 
+              100 * apply((trainingPredict == yVec), 2, mean)))
+returnCode <- ReadKey()
 
 # Display example images along with predictions from neural network
-randIndices = randperm(numTrainEx,numTrainEx)
-for(exampleIndex in 1:10) {
+randIndices <- randperm(numTrainEx, numTrainEx)
+for (exampleIndex in 1:10) {
   print(sprintf("Displaying Example Image"))
-  xMatSel = subset(xMat,(rownames(xMat)) %in% randIndices[exampleIndex])
-  returnCode <- displayData(xMatSel)
-  examplePredict <- (predict(theta1Mat,theta2Mat,xMatSel))
-  print(sprintf("Neural Network Prediction: %d (digit %d)",examplePredict,examplePredict%%10))
-  returnCode <- readKey()
+  xMatSel <- subset(xMat, (rownames(xMat)) %in% randIndices[exampleIndex])
+  returnCode <- DisplayData(xMatSel)
+  examplePredict <- (Predict(theta1Mat, theta2Mat, xMatSel))
+  print(sprintf("Neural Network Prediction: %d (digit %d)", examplePredict, 
+                examplePredict %% 10))
+  returnCode <- ReadKey()
 }
