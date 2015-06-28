32868e6b6249d7be72bd3a2838ab7c89e50be3d8
cklcit03 <flashbuzzer@gmail.com>
2015-06-27 22:44:37 -0700

    conform to Google's R style guide

--- programming_exercise_6/R/ex6.R
+++ programming_exercise_6/R/ex6.R
@@ -20,124 +20,185 @@
 # Load packages
 library(e1071)
 
-# Read key press
-readKey <- function(){
-  cat ("Program paused. Press enter to continue.")
+ReadKey <- function() {
+  # Reads key press.
+  #
+  # Args:
+  #   None.
+  #
+  # Returns:
+  #   None.
+  cat("Program paused. Press enter to continue.")
   line <- readline()
   return(0)
 }
 
-# Plot data
-plotData <- function(X,y){
-  positiveIndices = which(y == 1)
-  negativeIndices = which(y == 0)
-  positiveExamples = cbind(X[positiveIndices,])
-  negativeExamples = cbind(X[negativeIndices,])
-  plot(positiveExamples[,1],positiveExamples[,2],cex=1.75,pch="+",xlab="",ylab="")
-  points(x=negativeExamples[,1],y=negativeExamples[,2],col="black",bg="yellow",cex=1.75,pch=21,xlab="",ylab="")
+PlotData <- function(X, y) {
+  # Plots data.
+  #
+  # Args:
+  #   X: Features to be plotted.
+  #   y: Data labels.
+  #
+  # Returns:
+  #   None.
+  positiveIndices <- which(y == 1)
+  negativeIndices <- which(y == 0)
+  positiveExamples <- cbind(X[positiveIndices, ])
+  negativeExamples <- cbind(X[negativeIndices, ])
+  plot(positiveExamples[, 1], positiveExamples[, 2], cex=1.75, pch="+",
+       xlab="", ylab="")
+  points(x=negativeExamples[, 1], y=negativeExamples[, 2], col="black",
+         bg="yellow", cex=1.75, pch=21, xlab="", ylab="")
   return(0)
 }
 
-# Plot linear decision boundary learned via SVM
-visualizeBoundaryLinear <- function(X,y,model){
-  returnCode <- plotData(X,y)
-  weightVec = t(model$coefs) %*% model$SV
-  offsetVal = -model$rho
-  xVals = seq(min(X[,1]),max(X[,1]),length=100)
-  yLineVals = (-1)*(weightVec[,1]*xVals+offsetVal)/weightVec[,2]
-  lines(xVals,yLineVals,col="blue",pch="-")
+VisualizeBoundaryLinear <- function(X, y, model) {
+  # Plots linear decision boundary learned via SVM.
+  #
+  # Args:
+  #   X: Features to be plotted.
+  #   y: Data labels.
+  #   model: Fitted SVM model.
+  #
+  # Returns:
+  #   None.
+  returnCode <- PlotData(X, y)
+  weightVec <- t(model$coefs) %*% model$SV
+  offsetVal <- -model$rho
+  xVals <- seq(min(X[, 1]), max(X[, 1]), length=100)
+  yLineVals <- (-1) * (weightVec[, 1] * xVals + offsetVal) / weightVec[, 2]
+  lines(xVals, yLineVals, col="blue", pch="-")
   return(0)
 }
 
-# Plot non-linear decision boundary learned via SVM
-visualizeBoundary <- function(X,y,model){
-  returnCode <- plotData(X,y)
-  x1 = seq(min(X[,1]),max(X[,1]),length=100)
-  x2 = seq(min(X[,2]),max(X[,2]),length=100)
-  valsMat = mat.or.vec(dim(cbind(x1))[1],dim(cbind(x2))[1])
-  for(index0 in 1:dim(cbind(x1))[1]) {
-    for(index1 in 1:dim(cbind(x2))[1]) {
-      valsMat[index0,index1] <- predict(model,cbind(x1[index0],x2[index1]))
+VisualizeBoundary <- function(X, y, model) {
+  # Plots non-linear decision boundary learned via SVM.
+  #
+  # Args:
+  #   X: Features to be plotted.
+  #   y: Data labels.
+  #   model: Fitted SVM model.
+  #
+  # Returns:
+  #   None.
+  returnCode <- PlotData(X, y)
+  x1 <- seq(min(X[, 1]), max(X[, 1]), length=100)
+  x2 <- seq(min(X[, 2]), max(X[, 2]), length=100)
+  valsMat <- mat.or.vec(dim(cbind(x1))[1], dim(cbind(x2))[1])
+  for (index0 in 1:dim(cbind(x1))[1]) {
+    for (index1 in 1:dim(cbind(x2))[1]) {
+      valsMat[index0, index1] <- predict(model, cbind(x1[index0], x2[index1]))
     }
   }
-  contour(x=x1,y=x2,valsMat,nlevels=1,col="blue",lwd=2,drawlabels=FALSE,add=TRUE)
+  contour(x=x1, y=x2, valsMat, nlevels=1, col="blue", lwd=2, drawlabels=FALSE,
+          add=TRUE)
   return(0)
 }
 
-# Select optimal learning parameters for radial basis SVM
-dataset3Params <- function(X1,y1,Xval,yVal){
-  C = 1
-  sigma = 0.3
-  cArr = cbind(0.01,0.03,0.1,0.3,1,3,10,30)
-  sigmaArr = cbind(0.01,0.03,0.1,0.3,1,3,10,30)
-  bestPredErr = 1000000
-  for(cIndex in 1:dim(cArr)[2]) {
-    for(sigmaIndex in 1:dim(sigmaArr)[2]) {
-      svmModelTmp <- svm(X1,y=y1,scale=FALSE,type="C-classification",kernel="radial",gamma=1/(2*(sigmaArr[,sigmaIndex]^2)),cost=cArr[,cIndex])
-      predVec <- predict(svmModelTmp,Xval)
-      currPredErr <- apply((predVec != yVal),2,mean)
+Dataset3Params <- function(X1, y1, Xval, yVal) {
+  # Selects optimal learning parameters for radial basis SVM.
+  #
+  # Args:
+  #   X1: Matrix of training features.
+  #   y1: Vector of training labels.
+  #   Xval: Matrix of cross-validation features.
+  #   yval: Vector of cross-validation labels.
+  #
+  # Returns:
+  #   returnList: List of two objects.
+  #               C: Best (in terms of minimum prediction error for 
+  #                  cross-validation data) value that controls penalty for 
+  #                  misclassified training examples.
+  #               sigma: Best (in terms of minimum prediction error for 
+  #                      cross-validation data) value that determines how fast
+  #                      similarity metric decreases as examples are further 
+  #                      apart.
+  C <- 1
+  sigma <- 0.3
+  cArr <- cbind(0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30)
+  sigmaArr <- cbind(0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30)
+  bestPredErr <- 1000000
+  for (cIndex in 1:dim(cArr)[2]) {
+    for (sigmaIndex in 1:dim(sigmaArr)[2]) {
+      svmModelTmp <- svm(X1, y=y1, scale=FALSE, type="C-classification", 
+                         kernel="radial", 
+                         gamma=1 / (2 * (sigmaArr[, sigmaIndex] ^ 2)), 
+                         cost=cArr[, cIndex])
+      predVec <- predict(svmModelTmp, Xval)
+      currPredErr <- apply((predVec != yVal), 2, mean)
       if (currPredErr < bestPredErr) {
-        cBest = cArr[,cIndex]
-        sigmaBest = sigmaArr[,sigmaIndex]
-        bestPredErr = currPredErr
+        cBest <- cArr[, cIndex]
+        sigmaBest <- sigmaArr[, sigmaIndex]
+        bestPredErr <- currPredErr
       }
     }
   }
-  C = cBest
-  sigma = sigmaBest
-  returnList = list("C"=C,"sigma"=sigma)
+  C <- cBest
+  sigma <- sigmaBest
+  returnList <- list("C"=C, "sigma"=sigma)
   return(returnList)
 }
 
-# Use setwd() to set working directory to directory that contains this source file
+# Use setwd() to set working directory to directory that contains this source 
+# file
 # Load file into R
 print(sprintf("Loading and Visualizing Data ..."))
-exampleData1 = read.csv("../exampleData1.txt",header=FALSE)
-xMat = cbind(exampleData1[,"V1"],exampleData1[,"V2"])
-yVec = cbind(exampleData1[,"V3"])
+exampleData1 <- read.csv("../exampleData1.txt", header=FALSE)
+xMat <- cbind(exampleData1[, "V1"], exampleData1[, "V2"])
+yVec <- cbind(exampleData1[, "V3"])
 
 # Plot data
-returnCode <- plotData(cbind(exampleData1[,"V1"],exampleData1[,"V2"]),exampleData1[,"V3"])
-returnCode <- readKey()
+returnCode <- PlotData(cbind(exampleData1[, "V1"], exampleData1[, "V2"]),
+                       exampleData1[, "V3"])
+returnCode <- ReadKey()
 
 # Train linear SVM on data
 print(sprintf("Training Linear SVM ..."))
-svmModel <- svm(xMat,y=yVec,scale=FALSE,type="C-classification",kernel="linear")
-returnCode <- visualizeBoundaryLinear(xMat,yVec,svmModel)
-returnCode <- readKey()
+svmModel <- svm(xMat, y=yVec, scale=FALSE, type="C-classification", 
+                kernel="linear")
+returnCode <- VisualizeBoundaryLinear(xMat, yVec, svmModel)
+returnCode <- ReadKey()
 
 # Load next file into R
 print(sprintf("Loading and Visualizing Data ..."))
-exampleData2 = read.csv("../exampleData2.txt",header=FALSE)
-xMat2 = cbind(exampleData2[,"V1"],exampleData2[,"V2"])
-yVec2 = cbind(exampleData2[,"V3"])
+exampleData2 <- read.csv("../exampleData2.txt", header=FALSE)
+xMat2 <- cbind(exampleData2[, "V1"], exampleData2[, "V2"])
+yVec2 <- cbind(exampleData2[, "V3"])
 
 # Plot data
-returnCode <- plotData(cbind(exampleData2[,"V1"],exampleData2[,"V2"]),exampleData2[,"V3"])
-returnCode <- readKey()
+returnCode <- PlotData(cbind(exampleData2[, "V1"], exampleData2[, "V2"]),
+                       exampleData2[, "V3"])
+returnCode <- ReadKey()
 
 # Train radial basis SVM on data
-print(sprintf("Training SVM with RBF Kernel (this may take 1 to 2 minutes) ..."))
-sigmaVal = 0.1
-svmModel2 <- svm(xMat2,y=yVec2,scale=FALSE,type="C-classification",kernel="radial",gamma=1/(2*(sigmaVal^2)))
-returnCode <- visualizeBoundary(xMat2,yVec2,svmModel2)
-returnCode <- readKey()
+print(sprintf("Training SVM with RBF Kernel (this may take 1 to 2 minutes) 
+              ..."))
+kSigmaVal <- 0.1
+svmModel2 <- svm(xMat2, y=yVec2, scale=FALSE, type="C-classification", 
+                 kernel="radial", gamma=1 / (2 * (kSigmaVal ^ 2)))
+returnCode <- VisualizeBoundary(xMat2, yVec2, svmModel2)
+returnCode <- ReadKey()
 
 # Load next file into R
 print(sprintf("Loading and Visualizing Data ..."))
-exampleData3 = read.csv("../exampleData3.txt",header=FALSE)
-exampleValData3 = read.csv("../exampleValData3.txt",header=FALSE)
-xMat3 = cbind(exampleData3[,"V1"],exampleData3[,"V2"])
-yVec3 = cbind(exampleData3[,"V3"])
-xValMat3 = cbind(exampleValData3[,"V1"],exampleValData3[,"V2"])
-yValVec3 = cbind(exampleValData3[,"V3"])
+exampleData3 <- read.csv("../exampleData3.txt", header=FALSE)
+exampleValData3 <- read.csv("../exampleValData3.txt", header=FALSE)
+xMat3 <- cbind(exampleData3[, "V1"], exampleData3[, "V2"])
+yVec3 <- cbind(exampleData3[, "V3"])
+xValMat3 <- cbind(exampleValData3[, "V1"], exampleValData3[, "V2"])
+yValVec3 <- cbind(exampleValData3[, "V3"])
 
 # Plot data
-returnCode <- plotData(cbind(exampleData3[,"V1"],exampleData3[,"V2"]),exampleData3[,"V3"])
-returnCode <- readKey()
+returnCode <- PlotData(cbind(exampleData3[, "V1"], exampleData3[, "V2"]),
+                       exampleData3[, "V3"])
+returnCode <- ReadKey()
 
 # Train radial basis SVM on data
-dataset3ParamsList <- dataset3Params(xMat3,yVec3,xValMat3,yValVec3)
-svmModel3 <- svm(xMat3,y=yVec3,scale=FALSE,type="C-classification",kernel="radial",gamma=1/(2*(dataset3ParamsList$sigma^2)),cost=dataset3ParamsList$C)
-returnCode <- visualizeBoundary(xMat3,yVec3,svmModel3)
-returnCode <- readKey()
+dataset3ParamsList <- Dataset3Params(xMat3, yVec3, xValMat3, yValVec3)
+svmModel3 <- svm(xMat3, y=yVec3, scale=FALSE, type="C-classification", 
+                 kernel="radial", 
+                 gamma=1 / (2 * (dataset3ParamsList$sigma ^ 2)), 
+                 cost=dataset3ParamsList$C)
+returnCode <- VisualizeBoundary(xMat3, yVec3, svmModel3)
+returnCode <- ReadKey()
--- programming_exercise_6/R/ex6_spam.R
+++ programming_exercise_6/R/ex6_spam.R
@@ -21,153 +21,202 @@
 library(e1071)
 library(Rstem)
 
-# Read key press
-readKey <- function(){
-  cat ("Program paused. Press enter to continue.")
+ReadKey <- function() {
+  # Reads key press.
+  #
+  # Args:
+  #   None.
+  #
+  # Returns:
+  #   None.
+  cat("Program paused. Press enter to continue.")
   line <- readline()
   return(0)
 }
 
-# Return array of words in a vocabulary list
-getVocabList <- function(){
-  fullList = as.matrix(scan("../vocab.txt",what=character()))
-  vocabList = matrix(0,(dim(fullList)[1]/2),1)
-  for(listIndex in 1:(dim(fullList)[1]/2)) {
-    vocabList[listIndex,] = fullList[2*listIndex]
+GetVocabList <- function() {
+  # Returns array of words in a vocabulary list.
+  #
+  # Args:
+  #   None.
+  #
+  # Returns:
+  #   vocabList: Matrix that contains a vocabulary list.
+  fullList <- as.matrix(scan("../vocab.txt", what=character()))
+  numWords <- (dim(fullList)[1]) / 2
+  if (numWords > 0) {
+    vocabList <- matrix(0, numWords, 1)
+    for (listIndex in 1:numWords) {
+      vocabList[listIndex, ] <- fullList[2 * listIndex]
+    }
+  } else {
+    stop('Insufficient number of words in vocabulary list')
   }
   return(vocabList)
 }
 
-# Preprocess e-mail and return list of word indices
-processEmail <- function(fileContents){
-  vocabList <- getVocabList()
-  lowerFilCont = matrix(0,dim(fileContents)[1],1)
-  for(listIndex in 1:dim(fileContents)[1]) {
-    lowerFilCont[listIndex,] = tolower(fileContents[listIndex,])
-    if (lowerFilCont[listIndex,] == '\n') {
-      lowerFilCont[listIndex,] = ' '
-    }
-  }
-  lowerFilContStr = lowerFilCont[1]
-  for(listIndex in 2:dim(lowerFilCont)[1]) {
-    lowerFilContStr = paste(lowerFilContStr,lowerFilCont[listIndex],sep='')
-  }
-  modFileContents = gsub("<[^<>]+>"," ",lowerFilContStr)
-  modFileContents = gsub("[0-9]+","number",modFileContents)
-  modFileContents = gsub("(http|https)://[^ ]+","httpaddr",modFileContents)
-  modFileContents = gsub("[^ ]+@[^ ]+","emailaddr",modFileContents)
-  modFileContents = gsub("[$]+","dollar",modFileContents)
-  print(sprintf("==== Processed Email ===="))
-  
-  # Remove punctuation in processed e-mail
-  modFileContents=gsub("[[:punct:]]","",modFileContents)
-  wordsModFileContents=strsplit(modFileContents," ")
-  wordIndices = list()
-  for(wordIndex in 1:length(wordsModFileContents[[1]])) {
-    if (wordsModFileContents[[1]][wordIndex] != "") {
-      currWord = gsub("[^a-zA-Z0-9]","",wordsModFileContents[[1]][wordIndex])
-      
-      # Apply porterStemmer in Rstem package
-      stemCurrWord = wordStem(currWord)
-      if (nchar(stemCurrWord) >= 1) {
-        
-        # Search through vocabList for stemmed word
-        for(vocabListIndex in 1:length(vocabList)) {
-          if (stemCurrWord == vocabList[vocabListIndex]) {
-            wordIndices = rbind(wordIndices,vocabListIndex)
-          }
-        }
-        
-        # Display stemmed word
-        print(sprintf("%s",stemCurrWord))
+ProcessEmail <- function(fileContents) {
+  # Preprocesses e-mail and returns list of word indices.
+  #
+  # Args:
+  #   fileContents: Vector of characters from an e-mail in a text file.
+  #
+  # Returns:
+  #   wordIndices: Vector of indices of processed words in a vocabulary list.
+  numChars <- dim(fileContents)[1]
+  if (numChars > 0) {
+    lowerFilCont <- matrix(0, numChars, 1)
+    for (listIndex in 1:numChars) {
+      lowerFilCont[listIndex, ] <- tolower(fileContents[listIndex, ])
+      if (lowerFilCont[listIndex, ] == '\n') {
+        lowerFilCont[listIndex, ] <- ' '
       }
     }
+    lowerFilContStr <- lowerFilCont[1]
+    for (listIndex in 2:numChars) {
+      lowerFilContStr <- paste(lowerFilContStr, lowerFilCont[listIndex], 
+                               sep='')
+    }
+    modFileContents <- gsub("<[^<>]+>", " ", lowerFilContStr)
+    modFileContents <- gsub("[0-9]+", "number", modFileContents)
+    modFileContents <- gsub("(http|https)://[^ ]+", "httpaddr", 
+                            modFileContents)
+    modFileContents <- gsub("[^ ]+@[^ ]+", "emailaddr", modFileContents)
+    modFileContents <- gsub("[$]+", "dollar", modFileContents)
+    print(sprintf("==== Processed Email ===="))
+
+    # Remove punctuation in processed e-mail
+    modFileContents <- gsub("[[:punct:]]", "", modFileContents)
+    wordsModFileContents <- strsplit(modFileContents, " ")
+    vocabList <- GetVocabList()
+    wordIndices <- list()
+    for (wordIndex in 1:length(wordsModFileContents[[1]])) {
+      if (wordsModFileContents[[1]][wordIndex] != "") {
+        currWord <- gsub("[^a-zA-Z0-9]", "", 
+                         wordsModFileContents[[1]][wordIndex])
+
+        # Apply porterStemmer in Rstem package
+        stemCurrWord <- wordStem(currWord)
+        if (nchar(stemCurrWord) >= 1) {
+
+          # Search through vocabList for stemmed word
+          for (vocabListIndex in 1:length(vocabList)) {
+            if (stemCurrWord == vocabList[vocabListIndex]) {
+              wordIndices <- rbind(wordIndices, vocabListIndex)
+            }
+          }
+
+          # Display stemmed word
+          print(sprintf("%s", stemCurrWord))
+        }
+      }
+    }
+    print(sprintf("========================="))
+  } else {
+    stop('Insufficient number of characters in e-mail')
   }
-  print(sprintf("========================="))
-  
   return(wordIndices)
 }
 
-# Process list of word indices and return feature vector
-emailFeatures <- function(emailWordIndices){
-  numDictWords = 1899
-  featuresVec = matrix(0,numDictWords,1)
-  for(wordIndex in 1:length(emailWordIndices)){
-    featuresVec[as.numeric(emailWordIndices[wordIndex])] = 1
+EmailFeatures <- function(emailWordIndices) {
+  # Processes list of word indices and returns feature vector.
+  #
+  # Args:
+  #   emailWordIndices: Vector of indices of processed words (from an e-mail) 
+  #                     in a vocabulary list.
+  #
+  # Returns:
+  #   featuresVec: Vector of booleans where the i-th entry indicates whether
+  #                the i-th word in the vocabulary list occurs in the e-mail of
+  #                interest.
+  kNumDictWords <- 1899
+  featuresVec <- matrix(0, kNumDictWords, 1)
+  numEmailWordIndices <- length(emailWordIndices)
+  if (numEmailWordIndices > 0) {
+    for (wordIndex in 1:length(emailWordIndices)) {
+      featuresVec[as.numeric(emailWordIndices[wordIndex])] <- 1
+    }
+  } else {
+    stop('Insufficient number of processed word indices')
   }
-  
   return(featuresVec)
 }
 
-# Use setwd() to set working directory to directory that contains this source file
+# Use setwd() to set working directory to directory that contains this source 
+# file
 # Load file into R
 print(sprintf("Preprocessing sample email (emailSample1.txt)"))
-emailSample1 <- file("../emailSample1.txt","rb")
-fileContents = readChar(emailSample1,1)
-for(cIndex in 2:file.info("../emailSample1.txt")$size) {
-  fileContents = rbind(fileContents,readChar(emailSample1,1))
+emailSample1 <- file("../emailSample1.txt", "rb")
+fileContents <- readChar(emailSample1, 1)
+for (cIndex in 2:file.info("../emailSample1.txt")$size) {
+  fileContents <- rbind(fileContents, readChar(emailSample1, 1))
 }
 close(emailSample1)
 
 # Extract features from file
-emailWordIndices <- processEmail(fileContents)
+emailWordIndices <- ProcessEmail(fileContents)
 print(sprintf("Word Indices: "))
-for(wordIndex in 1:length(emailWordIndices)) {
-  print(sprintf("%d",as.numeric(emailWordIndices[wordIndex])))
+for (wordIndex in 1:length(emailWordIndices)) {
+  print(sprintf("%d", as.numeric(emailWordIndices[wordIndex])))
 }
-returnCode <- readKey()
+returnCode <- ReadKey()
 print(sprintf("Extracting features from sample email (emailSample1.txt)"))
-emailSample1 <- file("../emailSample1.txt","rb")
-fileContents = readChar(emailSample1,1)
-for(cIndex in 2:file.info("../emailSample1.txt")$size) {
-  fileContents = rbind(fileContents,readChar(emailSample1,1))
+emailSample1 <- file("../emailSample1.txt", "rb")
+fileContents <- readChar(emailSample1, 1)
+for (cIndex in 2:file.info("../emailSample1.txt")$size) {
+  fileContents <- rbind(fileContents, readChar(emailSample1, 1))
 }
 close(emailSample1)
-emailWordIndices <- processEmail(fileContents)
-featuresVec <- emailFeatures(emailWordIndices)
-print(sprintf("Length of feature vector: %d",length(featuresVec)))
-print(sprintf("Number of non-zero entries: %d",sum(featuresVec > 0)))
-returnCode <- readKey()
+emailWordIndices <- ProcessEmail(fileContents)
+featuresVec <- EmailFeatures(emailWordIndices)
+print(sprintf("Length of feature vector: %d", length(featuresVec)))
+print(sprintf("Number of non-zero entries: %d", sum(featuresVec > 0)))
+returnCode <- ReadKey()
 
 # Train a linear SVM for spam classification
-spamTrain = read.csv("../spamTrain.txt",header=FALSE)
-xMat = as.matrix(subset(spamTrain,select=-c(V1900)))
-yVec = as.vector(subset(spamTrain,select=c(V1900)))
+spamTrain <- read.csv("../spamTrain.txt", header=FALSE)
+xMat <- as.matrix(subset(spamTrain, select=-c(V1900)))
+yVec <- as.vector(subset(spamTrain, select=c(V1900)))
 print(sprintf("Training Linear SVM (Spam Classification)"))
 print(sprintf("(this may take 1 to 2 minutes) ..."))
-svmModel <- svm(xMat,y=yVec,scale=FALSE,type="C-classification",kernel="linear",cost=0.1)
-svmPred <- predict(svmModel,xMat)
-print(sprintf("Training Accuracy: %.6f",100*apply(((as.numeric(svmPred)-1) == yVec),2,mean)))
+svmModel <- svm(xMat, y=yVec, scale=FALSE, type="C-classification", 
+                kernel="linear", cost=0.1)
+svmPred <- predict(svmModel, xMat)
+print(sprintf("Training Accuracy: %.6f", 
+              100 * apply(((as.numeric(svmPred) - 1) == yVec), 2, mean)))
 
 # Test this linear spam classifier
-spamTest = read.csv("../spamTest.txt",header=FALSE)
-xTestMat = as.matrix(subset(spamTest,select=-c(V1900)))
-yTestVec = as.vector(subset(spamTest,select=c(V1900)))
+spamTest <- read.csv("../spamTest.txt", header=FALSE)
+xTestMat <- as.matrix(subset(spamTest, select=-c(V1900)))
+yTestVec <- as.vector(subset(spamTest, select=c(V1900)))
 print(sprintf("Evaluating the trained Linear SVM on a test set ..."))
-testPred <- predict(svmModel,xTestMat)
-print(sprintf("Test Accuracy: %.6f",100*apply(((as.numeric(testPred)-1) == yTestVec),2,mean)))
-returnCode <- readKey()
+testPred <- predict(svmModel, xTestMat)
+print(sprintf("Test Accuracy: %.6f", 
+              100 * apply(((as.numeric(testPred) - 1) == yTestVec), 2, mean)))
+returnCode <- ReadKey()
 
 # Determine the top predictors of spam
-svmModelWeights = t(svmModel$coefs)%*%(svmModel$SV)
-svmModelWeightsSorted=sort(svmModelWeights,decreasing=TRUE,index.return=TRUE)
-vocabList <- getVocabList()
+svmModelWeights <- t(svmModel$coefs) %*% (svmModel$SV)
+svmModelWeightsSorted <- sort(svmModelWeights, decreasing=TRUE, 
+                              index.return=TRUE)
+vocabList <- GetVocabList()
 print(sprintf("Top predictors of spam:"))
-for(predIndex in 1:15) {
-  print(sprintf("%s (%.6f)",vocabList[svmModelWeightsSorted$ix[predIndex]],svmModelWeightsSorted$x[predIndex]))
+for (predIndex in 1:15) {
+  print(sprintf("%s (%.6f)", vocabList[svmModelWeightsSorted$ix[predIndex]], 
+                svmModelWeightsSorted$x[predIndex]))
 }
-returnCode <- readKey()
+returnCode <- ReadKey()
 
 # Test this linear spam classifier on another e-mail
-spamSample1 <- file("../spamSample1.txt","rb")
-fileContents = readChar(spamSample1,1)
-for(cIndex in 2:file.info("../spamSample1.txt")$size) {
-  fileContents = rbind(fileContents,readChar(spamSample1,1))
+spamSample1 <- file("../spamSample1.txt", "rb")
+fileContents <- readChar(spamSample1, 1)
+for (cIndex in 2:file.info("../spamSample1.txt")$size) {
+  fileContents <- rbind(fileContents, readChar(spamSample1, 1))
 }
 close(spamSample1)
-emailWordIndices <- processEmail(fileContents)
-featuresVec <- emailFeatures(emailWordIndices)
-svmPred <- predict(svmModel,t(featuresVec))
+emailWordIndices <- ProcessEmail(fileContents)
+featuresVec <- EmailFeatures(emailWordIndices)
+svmPred <- predict(svmModel, t(featuresVec))
 print(sprintf("Processed spamSample1.txt"))
-print(sprintf("Spam Classification: %d",as.numeric(svmPred)-1))
+print(sprintf("Spam Classification: %d", as.numeric(svmPred) - 1))
 print(sprintf("(1 indicates spam, 0 indicates not spam)"))
